f944c0bfe3293dd772b21b764af5b70e72bb183c
bu119 <109335452+bu119@users.noreply.github.com>
2023-04-01 13:36:43 +0900

    [Gold IV] Title: 우체국, Time: 200 ms, Memory: 44060 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.py
+++ 백준/Gold/2141. 우체국/우체국.py
@@ -1,14 +1,15 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+n = int(input().strip())
+
 people = {}
 num = 0
 for _ in range(n):
     x, a = map(int, input().split())
     people[x] = a
     num += a
-
-ans = 0
 ssum = 0
-
+ans = 0
 for key in sorted(people):
     ssum += people[key]
     if ssum >= num/2:
