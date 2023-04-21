4dad7ef2b78148ccb7dfc9a259caf0026388a89e
bu119 <109335452+bu119@users.noreply.github.com>
2023-04-22 00:48:37 +0900

    [Silver I] Title: 부분수열의 합, Time: 196 ms, Memory: 126488 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,20 @@
+def dfs(idx, ssum):
+
+    if idx == n:
+        return
+
+    visited[ssum+s[idx]] = 1
+    dfs(idx + 1, ssum)
+    dfs(idx + 1, ssum+s[idx])
+
+
 n = int(input())
 s = list(map(int, input().split()))
 total = sum(s)
-num = [0]*(total+2)
+visited = [0]*(total+2)
+dfs(0,0)
 
-for i in range(1<<n):
-    ssum = 0
-    for j in range(n):
-        if i & (1 << j):
-            ssum += s[j]
-    num[ssum] = 1
-
-for k in range(1, total+2):
-    if not num[k]:
-        print(k)
+for j in range(1, total+2):
+    if not visited[j]:
+        print(j)
         break
