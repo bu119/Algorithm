037e75e7dc2e6d270c09cc1fa8c3a17bafb604e5
bu119 <109335452+bu119@users.noreply.github.com>
2023-04-24 01:33:10 +0900

    [Silver I] Title: 회전 초밥, Time: 80 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-# 접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
+
 n, d, k, c = map(int, input().split())
 kind = []
 eat = dict()
@@ -20,16 +20,13 @@ cnt = 0
 end = k-1
 
 for start in range(n):
-    # print(eat)
     eat[c] = 1
     # 최대 종류
     cnt = max(cnt, len(eat))
-
     # 왼쪽 제거
     eat[kind[start]] -= 1
     if eat[kind[start]] == 0:
         del eat[kind[start]]
-
     # 한칸 옆으로
     end = (end+1) % n
     # 오른쪽 추가
