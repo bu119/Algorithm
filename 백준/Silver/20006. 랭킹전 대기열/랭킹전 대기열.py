f9a369426a2d08d515259d53cabbdecbcab34c96
bu119 <109335452+bu119@users.noreply.github.com>
2023-04-23 23:12:03 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 60 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -0,0 +1,32 @@
+p, m = map(int,input().split())
+# 플레이어수, 정원
+room = []
+for _ in range(p):
+    # 플레이어의 레벨 l, 닉네임 n
+    l, n = input().split()
+    if room:
+        flag = 1
+        for i in room:
+            num = int(i[0][0])
+            maxV = min(num+10, 500)
+            minV = max(1,num-10)
+            # 매칭가능한 방 있음
+            if minV <= int(l) <= maxV and len(i) < m:
+                i.append([l,n])
+                flag = 0
+                break
+        # 매칭 가능한 방 없음
+        if flag:
+            room.append([[l, n]])
+    else:
+        room.append([[l,n]])
+
+
+for j in room:
+    if len(j) == m:
+        print('Started!')
+    else:
+        print('Waiting!')
+
+    for k in sorted(j, key=lambda x: x[1]):
+        print(*k)
