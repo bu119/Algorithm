f5137bd57df10c1747ad6642f5d3522b6dbaa647
bu119 <109335452+bu119@users.noreply.github.com>
2023-04-30 20:13:31 +0900

    [Silver III] Title: 블로그, Time: 152 ms, Memory: 60696 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,18 +1,18 @@
 n, x = map(int,input().split())
 visitor = list(map(int,input().split()))
+ssum = sum(visitor[:x])
+ans = ssum
+cnt = 1
+for i in range(x, n):
+    ssum = ssum-visitor[i-x]+visitor[i]
+    if ans < ssum:
+        ans = ssum
+        cnt = 1
+    elif ans == ssum:
+        cnt += 1
 
-if max(visitor) == 0:
-    print("SAD")
-else:
-    ssum = sum(visitor[:x])
-    ans = ssum
-    cnt = 1
-    for i in range(x, n):
-        ssum = ssum-visitor[i-x]+visitor[i]
-        if ans < ssum:
-            ans = ssum
-            cnt = 1
-        elif ans == ssum:
-            cnt += 1
+if ans:
     print(ans)
     print(cnt)
+else:
+    print("SAD")
