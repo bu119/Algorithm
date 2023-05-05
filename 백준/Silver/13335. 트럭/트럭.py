28c87a36bed674a7083dc8da345e51d57f44ee40
bu119 <109335452+bu119@users.noreply.github.com>
2023-05-05 23:32:12 +0900

    [Silver I] Title: 트럭, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,25 +1,25 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-
 n, w, l = map(int,input().split())
-truck = deque(map(int,input().split()))
+truck = list(map(int,input().split()))
 # 다리 칸
-bridge = deque([0]*w)
+bridge = [0]*w
+bridgeW = 0
 time = 0
 while truck or bridge:
     # 시간 추가
     time += 1
     # 시간이 지나면 다리 맨 앞칸 무게 제거
-    bridge.popleft()
+    bridgeW -= bridge.pop(0)
 
     # 건너야 하는 트럭이 남아 있으면
     if truck:
         # 남은 트럭이 더해져 다리 하중보다 무게가 커지면 트럭 추가 안함
-        if sum(bridge) + truck[0] > l:
-            # 남은 트럭이 존재하면 추가
+        if bridgeW + truck[0] > l:
+            # 남은 트럭이 존재하면 0을 추가하여 다리위에 있는 트럭 보내기
             bridge.append(0)
         else:
-            # 안크면 다리에 트럭 추가
-            bridge.append(truck.popleft())
+            # 하증을 견딜수있으면
+            bridge.append(truck[0])
+            # 다리위 트럭 추가
+            bridgeW += truck[0]
+            truck.pop(0)
 print(time)
