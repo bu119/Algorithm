6f9ed129b4a7af7198c15cd167b5175db892eeca
bu119 <109335452+bu119@users.noreply.github.com>
2023-05-21 15:52:23 +0900

    [Silver II] Title: 한 줄로 서기, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -3,12 +3,11 @@ n = int(input())
 # 키가 1인사람부터 왼쪽에 키큰 사람이 몇명 있는지: 2 1 1 0
 arr = list(map(int,input().split()))
 ans = [0]*n
-ans[arr[0]] = 1
-for i in range(1, n):
+for i in range(n):
     cnt = 0
     for j in range(n):
         if cnt == arr[i] and ans[j] == 0:
-            ans[j] = i + 1
+            ans[j] = i+1
             break
         elif ans[j] == 0:
             cnt += 1
