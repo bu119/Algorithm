19f500dd36081779e1e178a02aca7c6744caeee2
bu119 <109335452+bu119@users.noreply.github.com>
2023-06-02 17:29:53 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+dp = [1] * 10001
+for i in range(2, 10001):
+    dp[i] += dp[i-2]
+for j in range(3, 10001):
+    dp[j] += dp[j-3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
