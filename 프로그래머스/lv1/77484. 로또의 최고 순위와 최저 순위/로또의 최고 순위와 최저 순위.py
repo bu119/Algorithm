af6c44eb001fa1a5a4e3eb301fa8e2078139e062
bu119 <109335452+bu119@users.noreply.github.com>
2023-06-19 17:11:37 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,21 +1,8 @@
-def check(cnt):
-    if cnt == 6:
-        return 1
-    elif cnt == 5:
-        return 2
-    elif cnt == 4:
-        return 3
-    elif cnt == 3:
-        return 4
-    elif cnt == 2:
-        return 5
-    else:
-        return 6
-
-    
 def solution(lottos, win_nums):
     # 알아볼 수 없는 번호를 0으로 표기
     answer = []
+    check = [6,6,5,4,3,2,1]
+    
     zeroCnt = lottos.count(0)
     set_lottos = set(lottos)
     set_win = set(win_nums)
@@ -24,7 +11,7 @@ def solution(lottos, win_nums):
     if len(minV) == 6:
         return [1,1]
     
-    answer.append(check(len(minV)+zeroCnt))
-    answer.append(check(len(minV)))
+    answer.append(check[len(minV)+zeroCnt])
+    answer.append(check[len(minV)])
     
     return answer
