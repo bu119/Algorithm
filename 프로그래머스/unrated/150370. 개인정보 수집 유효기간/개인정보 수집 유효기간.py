58dfa0e58606428f2ee6bb8c4c7840244a4c602c
bu119 <109335452+bu119@users.noreply.github.com>
2023-06-19 17:27:05 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.13 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,40 +1,18 @@
 def solution(today, terms, privacies):
-    tyy, tmm, tdd = map(int, today.split("."))
-
     answer = []
     term = {}
     for i in terms:
         type, month = i.split()
         term[type] = int(month)
-   
+    
+    tyy, tmm, tdd = map(int, today.split("."))
+    today_day = tdd + tmm*28 + tyy*12*28
+    
     for j in range(len(privacies)):
         date, type = privacies[j].split()
         yy, mm, dd = map(int, date.split("."))
+        check_day = dd + mm*28 + yy*12*28 + term[type]*28
         
-        # 유효기간 계산
-        mm += term[type]
-        
-        if mm > 12:
-            yy += mm//12
-            mm %= 12
-            
-        if mm == 0:
-            yy -= 1
-            mm = 12
-                
-        dd -= 1
-        if dd == 0:
-            mm -= 1
-            dd = 28
-        
-        if tyy > yy:
+        if check_day <= today_day:
             answer.append(j+1)
-        elif tyy == yy:
-            if tmm > mm:
-                answer.append(j+1)
-            elif tmm == mm:
-                if tdd > dd:
-                    answer.append(j+1)
-            
-
     return answer
