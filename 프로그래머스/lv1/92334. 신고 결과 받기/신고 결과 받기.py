3bfdc9ec18586815237e4113bffc5a7fb66b479b
bu119 <109335452+bu119@users.noreply.github.com>
2023-07-04 16:26:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,14 +1,4 @@
 def solution(id_list, report, k):
-    # 게시판 불량 이용자를 신고하고 처리 결과를 메일로 발송
-    # 한 번에 한 명의 유저를 신고 가능 #
-    # 동일한 유저에 대한 신고 횟수는 1회로 처리
-    # k번 이상 신고된 유저는 게시판 이용이 정지
-    # 해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송 #
-    # 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송
-    
-    # 이용자의 ID가 담긴 문자열 배열 id_list
-    # 각 이용자가 신고한 이용자의 ID 정보가 담긴 문자열 배열 report
-    # 정지 기준이 되는 신고 횟수 k
     
     reported_cnt = {}
     reported_me = {}
@@ -29,6 +19,7 @@ def solution(id_list, report, k):
         if k <= len(reported_me[id_list[i]]):
             # 나를 투표한 사람에게 메일을 날림
             for j in reported_me[id_list[i]]:
+                # 내가 받은 메일 수
                 reported_cnt[j] += 1
 
     return list(reported_cnt.values())
