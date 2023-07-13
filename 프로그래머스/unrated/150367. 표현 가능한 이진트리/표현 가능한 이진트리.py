4f236870ae1ea087824bea78074d2e7da339b2a6
bu119 <109335452+bu119@users.noreply.github.com>
2023-07-13 23:08:30 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 93.83 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,33 +1,44 @@
 import math
-def check(num_bin, prev_parent):
-    # 중앙값(자손) 기준으로 재귀적으로 확인
-    mid = len(num_bin) // 2
-    if num_bin: son = (num_bin[mid] == '1')
-    else: return True
-    
-    # 내가 존재하면 부모가 존재해야함.
-    if son and not prev_parent:
-        return False
-    else:
-        return check(num_bin[mid + 1:], son) and check(num_bin[:mid], son)
 
-def sol(num):
-    # 1은 항상 참
-    if num == 1: return 1
+def makeBinary(num):
+    if num == 1:
+        return 1
 
     # 2진수 변환
-    num_bin = bin(num)[2:]
-    # 2^n - 1꼴의 자릿수를 가져야함.
-    digit = 2 ** (int(math.log(len(num_bin), 2)) + 1) - 1
-    num_bin = "0" * (digit - len(num_bin)) + num_bin
-    # print(digit, num_bin)
+    binary = bin(num)[2:]
+    # 2^n - 1꼴의 자릿수
+    n = 2 ** (int(math.log(len(binary), 2)) + 1) - 1
+    binary = '0' * (n - len(binary)) + binary
+
+    if binary[len(binary) // 2] == '1' and checkTree(binary, True):
+        return 1
+    else:
+        return 0
     
-    # 누군가의 부모 노드는 항상 존재해야함. 
-    if num_bin[len(num_bin) // 2] == '1'and check(num_bin, True): return 1
-    else: return 0
+    
+def checkTree(binary, parent):
+    mid = len(binary) // 2
+    if binary:
+        if binary[mid] == '1':
+            son = True
+        else:
+            son = False
+    else:
+        return True
+
+    # 부모가 존재
+    if son and not parent:
+        return False
+    else:
+        return checkTree(binary[mid + 1:], son) and checkTree(binary[:mid], son)
+
 
 def solution(numbers):
     answer = []
+    # print(numbers)
     for num in numbers:
-        answer.append(sol(num))
+        binary = makeBinary(num)
+        # 부모 노드는 항상 존재
+        answer.append(makeBinary(num))
+
     return answer
