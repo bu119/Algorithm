91251ec3920662a6b0bf9c0b88b3be1f1fa45214
bu119 <109335452+bu119@users.noreply.github.com>
2023-07-17 20:14:56 +0900

    [level 3] Title: 블록 이동하기, Time: 16.56 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
+++ 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
@@ -42,30 +42,38 @@ def solution(board):
             if x1 == x2:
                 nx1 = x1 + k
                 nx2 = x2 + k
+                
+                # nx1(nx2)를 비교할때는 nx2(nx1)값이 대각선 체크가 된다.
                 if 0 <= nx1 < n and 0 <= nx2 < n and not board[nx1][y1] and not board[nx2][y2]:
+                    
                     # x1,y1 기준점 (x2,y2 위치가 x1 위아래 움직임, y1으로 대체)
-                    # 왼쪽 기준점
+                    # 왼쪽 기준점 (x1, y1 기준으로 회전)
                     if (nx1, y1, x1, y1) not in visited:
                         visited.add((nx1, y1, x1, y1))
                         visited.add((x1, y1, nx1, y1))
                         deq.append((nx1, y1, x1, y1, cnt + 1))
 
-                    # 오른쪽 기준점 (x2,y2 기준점)
+                    # 오른쪽 기준점 (x2,y2 기준으로 회전)
                     if (nx2, y2, x2, y2) not in visited:
                         visited.add((nx2, y2, x2, y2))
                         visited.add((x2, y2, nx2, y2))
                         deq.append((nx2, y2, x2, y2, cnt + 1))
 
-            # 세로로 있을 때
+            # 세로로 있을 때 (기준점에서 열만 움직이는 걸로 다른 점이 대체)
             if y1 == y2:
                 ny1 = y1 + k
                 ny2 = y2 + k
+                
+                # ny1(ny2)를 비교할때는 ny2(ny1)값이 대각선 체크가 된다.
                 if 0 <= ny1 < n and 0 <= ny2 < n and not board[x1][ny1] and not board[x2][ny2]:
-                    if (x1, ny1, x1, y1) not in visited:
-                        visited.add((x1, ny1, x1, y1))
+                    
+                    # x1,y1 기준으로 회전 (x2,y2 값 변화)
+                    if (x1, y1, x1, ny1) not in visited:
                         visited.add((x1, y1, x1, ny1))
-                        deq.append((x1, ny1, x1, y1, cnt + 1))
-
+                        visited.add((x1, ny1, x1, y1))
+                        deq.append((x1, y1, x1, ny1, cnt + 1))
+                        
+                    # x2,y2 기준으로 회전 (x1,y1 값 변화)
                     if (x2, ny2, x2, y2) not in visited:
                         visited.add((x2, ny2, x2, y2))
                         visited.add((x2, y2, x2, ny2))
