b6097a9dec75475210d8955d5154628eadf6969d
bu119 <109335452+bu119@users.noreply.github.com>
2023-07-31 00:01:19 +0900

    [Silver II] Title: 스택 수열, Time: 1164 ms, Memory: 35268 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -9,11 +9,15 @@ i = 1
 
 for num in arr:
 
+    # 입력한 수를 만날 때 까지 push
     while i <= num:
         stack.append(i)
         ans += '+'
         i += 1
 
+    # stack의 TOP이 입력한 수가 아니면 주어진 스택을 만들 수 없다.
+    # 오름차순으로 스택이 입력되는데 TOP이 num보다 크면
+    # num은 TOP보다 더 아래에 쌓여있기 때문
     if stack.pop() != num:
         print('NO')
         exit()
