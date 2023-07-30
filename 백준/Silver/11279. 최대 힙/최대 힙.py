fae772b75cc55bb7e89d2e6a45794ee415a12d41
bu119 <109335452+bu119@users.noreply.github.com>
2023-07-31 00:25:39 +0900

    [Silver II] Title: 최대 힙, Time: 120 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -2,15 +2,18 @@ import sys, heapq
 input = sys.stdin.readline
 
 n = int(input())
-heap = []
 
-for _ in range(n):
-    x = int(input())
+def solution():
+    heap = []
+    for _ in range(n):
+        x = int(input())
 
-    if x == 0:
-        if heap:
-            print(-heapq.heappop(heap))
+        if x == 0:
+            if heap:
+                print(-heapq.heappop(heap))
+            else:
+                print(0)
         else:
-            print(0)
-    else:
-        heapq.heappush(heap, -x)
+            heapq.heappush(heap, -x)
+
+solution()
