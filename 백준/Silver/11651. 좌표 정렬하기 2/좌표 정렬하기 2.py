11142327be23cfdd9f07f8c780193e658aaf35ee
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-08 23:46:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 508 ms, Memory: 133304 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
+ans = sorted(arr, key = lambda x :(x[1], x[0]))
 
-for a, b in sorted(arr, key = lambda x :(x[1], x[0])):
+for a, b in ans:
     print(a,b)
