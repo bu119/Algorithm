83728cc7f7bebd6e7870dd43670c18b126298284
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-11 00:20:21 +0900

    [Gold V] Title: 꿀 따기, Time: 144 ms, Memory: 125908 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.py
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.py
@@ -1,23 +1,26 @@
 n = int(input())
-honey = list(map(int, input().split()))
-reversedHoney = honey[::-1]
-total = sum(honey[1:n-1])
+arr = list(map(int, input().split()))
+# 두 마리 벌은 벌통으로 똑바로 날아가면서 지나가는 모든 칸에서 꿀을 딴다.
+# 벌이 시작한 장소에서는 어떤 벌도 꿀을 딸 수 없다.
+total = sum(arr)
 
-# 벌A 꿀통(이동) 벌B
-# 꿀통 위치만 두번 더 해진다.
-# 사이 값중 최대값만 비교
-moveBarrel = total + max(honey[1:n-1])
-# 벌A 벌B(이동) 꿀통
-moveB = total * 2 - honey[1]*2 + honey[n-1]*2
-# 꿀통 벌A(이동) 벌B (벌B 벌A(이동) 꿀통)
-moveA = total * 2 - reversedHoney[1]*2 + reversedHoney[n-1]*2
+moveB = total * 2 - arr[0]*2 - arr[1]*2
+moveBarrel = total - arr[0] - arr[n-1] # 더하기 꿀벌 위치
+moveA = total - arr[1] - arr[n-1] + arr[0]
 
-ans = max(moveA, moveB, moveBarrel)
+ans = max(moveA, moveB, moveBarrel+arr[1])
 
-for m in range(2, n-1):
-    moveB += honey[m-1] - honey[m]*2
-    moveA += reversedHoney[m-1] - reversedHoney[m]*2
+for m in range(2,n-1):
+    # 벌A 벌B(이동) 꿀통
+    moveB = moveB - arr[m]*2 + arr[m-1]
 
-    ans = max(ans, moveB, moveA)
+    # 꿀통 벌A(이동) 벌B
+    moveA = moveA + arr[m - 1] * 2 - arr[m]
+
+    # 벌A 꿀통(이동) 벌B
+    # 꿀통 위치만 두번 더 해진다.
+    # moveBarrel+arr[m]
+
+    ans = max(ans, moveA, moveB, moveBarrel+arr[m])
 
 print(ans)
