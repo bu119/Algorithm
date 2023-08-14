b4579cb558197957c1cb4be74196bc8c41ea4ca7
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-14 12:55:13 +0900

    [Gold II] Title: 저울, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -1,8 +1,10 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 weights = sorted(map(int,input().split()))
 # 누적합
 ssum = 1
-
 for weight in weights:
     if ssum < weight:
         break
