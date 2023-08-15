99b6729d077f39b397d817a73613f2cbaf21cde7
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-15 18:24:09 +0900

    [Silver II] Title: 나무 자르기, Time: 2068 ms, Memory: 150704 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,24 @@
+def binary_search(start, end):
+
+    while start <= end:
+
+        mid = (start + end) // 2
+        ssum = 0
+
+        for tree in trees:
+            if mid < tree:
+                ssum += tree-mid
+
+        if ssum >= m:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return end
+
+# 나무의 수 N과 상근이가 집으로 가져가려고 하는 나무의 길이 M
+n, m = map(int, input().split())
+trees = sorted(map(int, input().split()))
+start = 1
+end = trees[-1]
+print(binary_search(start, end))
