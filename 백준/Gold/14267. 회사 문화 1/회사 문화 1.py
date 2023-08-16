9ca04246c585fa41955cba1c776a6b99bf884389
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-16 16:17:17 +0900

    [Gold IV] Title: 회사 문화 1, Time: 196 ms, Memory: 42612 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -11,10 +11,8 @@ for _ in range(m):
     i, w = map(int, input().split())
     compliment[i] += w
 
-for i in range(1, n + 1):
-    # 사장이 아닐 때 칭찬 수치 계산
-    if i != 1:
-        # 상사가 가지고 있는 칭찬 수치를 부하직원에게 추가해줌
-        compliment[i] += compliment[superior[i]]
-        
-    print(compliment[i], end=' ')
+for i in range(2, n + 1):
+    # 상사가 가지고 있는 칭찬 수치를 부하직원에게 추가해줌
+    compliment[i] += compliment[superior[i]]
+
+print(*compliment[1:])
