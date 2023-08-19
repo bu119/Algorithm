6258378f536c05957acac5dac2b062d4addb9a6d
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-19 16:27:09 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,28 +1,27 @@
 import sys
 input=sys.stdin.readline
 
+def check(sentence):
+    stack = []
+
+    for i in sentence:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if not stack or stack.pop() != '(':
+                return 'no'
+        elif i == ']':
+            if not stack or stack.pop() != '[':
+                return 'no'
+
+    if stack:
+        return 'no'
+
+    return 'yes'
+
+
 while True:
     arr = input().rstrip()
     if arr == '.':
         break
-
-    if arr.count('(') != arr.count(')') or arr.count('[') != arr.count(']'):
-        print('no')
-        continue
-
-    check = ''
-
-    for i in arr:
-        if i in '()[]':
-            check += i
-
-    while "()" in check or "[]" in check:
-        if "()" in check:
-            check = check.replace("()", "")
-        if "[]" in check:
-            check = check.replace("[]", "")
-
-    if check:
-        print('no')
-    else:
-        print('yes')
+    print(check(arr))
