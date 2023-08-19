4e25df6878cd934e5794748cadffc91f3a098e6a
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-19 15:52:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 140 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,26 +1,27 @@
 import sys
 input=sys.stdin.readline
 
-arr = input().rstrip()
-while arr != '.':
+while True:
+    arr = input().rstrip()
+
+    if arr == '.':
+        break
 
     check = ''
     arr = ''.join(arr.split())
+    if arr.count('(') != arr.count(')') and arr.count('[') != arr.count(']'):
+        print('no')
+        continue
 
-    if len(arr) == 1:
-        print('yes')
+    for i in arr:
+        if i == '(' or i == ')' or i == '[' or i == ']':
+            check += i
+        n = len(check)
+        if n > 1:
+            if check[n - 2:n] == '()' or check[n - 2:n] == '[]':
+                check = check[:n - 2]
+
+    if check:
+        print('no')
     else:
-        for i in arr:
-            if i == '(' or i == ')' or i == '[' or i == ']':
-                check += i
-            n = len(check)
-            if n > 1:
-                if check[n - 2:n] == '()' or check[n - 2:n] == '[]':
-                    check = check[:n - 2]
-
-        if check:
-            print('no')
-        else:
-            print('yes')
-
-    arr = input().rstrip()
+        print('yes')
