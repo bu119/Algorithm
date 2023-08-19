c50cf9cc75d51e99d6dc65e021d8dfec7443a854
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-19 15:39:42 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 432 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,23 @@
+arr = input()
+while arr != '.':
+
+    check = ''
+    arr = ''.join(arr.split())
+
+    if len(arr) == 1:
+        print('yes')
+    else:
+        for i in arr:
+            if i == '(' or i == ')' or i == '[' or i == ']':
+                check += i
+            n = len(check)
+            if n > 1:
+                if check[n-2:n] == '()' or check[n-2:n] == '[]':
+                    check = check[:n-2]
+
+        if len(check):
+            print('no')
+        else:
+            print('yes')
+
+    arr = input()
