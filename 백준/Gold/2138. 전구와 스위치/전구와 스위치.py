1c344e50bf1799a3338becb8f3ebffbac16eb89a
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-22 13:03:23 +0900

    [Gold V] Title: 전구와 스위치, Time: 136 ms, Memory: 33672 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,12 +1,16 @@
 from copy import deepcopy
 
-def switch(bulb, arr):
+def switch(idx, bulb):
+
+    if idx == 0:
+        arr = [0, 1]
+    elif idx == n-1:
+        arr = [n-2, n-1]
+    else:
+        arr = [idx-1, idx, idx+1]
 
     for i in arr:
-        if bulb[i] == '0':
-            bulb[i] = '1'
-        else:
-            bulb[i] = '0'
+        bulb[i] = onOff[bulb[i]]
 
     return bulb
 
@@ -17,14 +21,7 @@ def change(bulb, cnt):
         # 0번 스위치를 고정하면
         # 0번을 변화 시킬수 있는건 1번 스위치 뿐이다.
         if bulb[k - 1] != result[k - 1]:
-
-            if k == n - 1:
-                arr = [n - 2, n - 1]
-                switch(bulb, arr)
-            else:
-                arr = [k - 1, k, k + 1]
-                switch(bulb, arr)
-
+            bulb = switch(k, bulb)
             cnt += 1
 
     if bulb[-1] == result[-1]:
@@ -35,10 +32,12 @@ def change(bulb, cnt):
 n = int(input())
 state = list(input())
 result = list(input())
+
+onOff = {'1': '0', '0': '1'}
 # 첫번째 스위치 안누르고 시작
 case1 = deepcopy(state)
 # 첫번째 스위치를 누르고 시작
-case2 = switch(state, [0,1])
+case2 = switch(0, state)
 
 ans = min(change(case1, 0), change(case2, 1))
 if ans == 100001:
