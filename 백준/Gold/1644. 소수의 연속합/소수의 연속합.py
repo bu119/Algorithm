c7d705370b0be629f5302873d90368b1a62570e3
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-23 14:05:05 +0900

    [Gold III] Title: 소수의 연속합, Time: 724 ms, Memory: 164460 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -4,6 +4,7 @@ n = int(input())
 is_prime_num = [True] * (n+1)
 is_prime_num[0] = False
 is_prime_num[1] = False
+
 prime_num = []
 
 for i in range(2, n+1):
@@ -18,17 +19,16 @@ end = 0
 ssum = 0
 
 while True:
-    try:
-        if ssum == n:
-            cnt += 1
-
-        if ssum < n:
-            ssum += prime_num[end]
-            end += 1
-        else:
-            ssum -= prime_num[start]
-            start += 1
-    except:
+    if ssum >= n:
+        ssum -= prime_num[start]
+        start += 1
+    elif end == len(prime_num):
         break
+    else:
+        ssum += prime_num[end]
+        end += 1
+
+    if ssum == n:
+        cnt += 1
 
 print(cnt)
