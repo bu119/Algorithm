061af17fbb715ddd3eca9cabca5c916810c09655
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-30 00:51:35 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 564 ms, Memory: 252364 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2(1).py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2(1).py
@@ -1,34 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(target):
-    s = 0
-    e = len(increase) - 1
-
-    # target 값과 같거나 큰 가장 작은 값을 찾아야 하기 때문에 일반 이분 탐색과는 조건이 살짝 다르다.
-    while s < e:
-        mid = (s + e) // 2
-
-        if increase[mid] == target:
-            return mid
-
-        elif increase[mid] > target:
-            e = mid
-        else:
-            s = mid + 1
-
-    return e
-
-
 n = int(input())
 arr = list(map(int,input().split()))
-increase = [arr[0]]
+increase = [0]
 
-for i in range(1, n):
-    if increase[-1] < arr[i]:
-        increase.append(arr[i])
-    elif increase[-1] > arr[i]:
-        idx = binary_search(arr[i])
-        increase[idx] = arr[i]
+for num in arr:
+    if increase[-1] < num:
+        increase.append(num)
 
-print(len(increase))
+    elif increase[-1] > num:
+        s = 0
+        e = len(increase) - 1
+
+        # num값과 같거나 큰 가장 작은 값을 찾아야 하기 때문에 일반 이분 탐색과는 조건이 살짝 다르다.
+        while s < e:
+            mid = (s + e) // 2
+
+            if increase[mid] < num:
+                s = mid + 1
+            else:
+                e = mid
+
+        increase[e] = num
+
+print(len(increase)-1)
