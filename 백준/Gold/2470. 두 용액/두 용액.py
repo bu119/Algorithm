efa97491af49ecd26cace799eaf02b1aa9407c73
bu119 <109335452+bu119@users.noreply.github.com>
2023-08-29 16:44:39 +0900

    [Gold V] Title: 두 용액, Time: 120 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,25 +1,28 @@
+def two_pointer():
+    s = 0
+    e = n - 1
+    minV = abs(solution[s] + solution[e])
+    minS = solution[s]
+    minE = solution[e]
+
+    # 투포인터 알고리즘
+    while s < e:
+        mixture = solution[s] + solution[e]
+        if minV == 0:
+            break
+
+        if abs(mixture) < minV:
+            minV = abs(mixture)
+            minS = solution[s]
+            minE = solution[e]
+        elif mixture < 0:
+            s += 1
+        else:
+            e -= 1
+
+    print(minS, minE)
+
+
 n = int(input())
 solution = sorted(map(int, input().split()))
-
-s = 0
-e = n-1
-minV = abs(solution[s] + solution[e])
-minS = solution[s]
-minE = solution[e]
-
-# 투포인터 알고리즘
-while s < e:
-    mixture = solution[s] + solution[e]
-    if minV == 0:
-        break
-
-    if abs(mixture) < minV:
-        minV = abs(mixture)
-        minS = solution[s]
-        minE = solution[e]
-    elif mixture < 0:
-        s += 1
-    else:
-        e -= 1
-
-print(minS, minE)
+two_pointer()
