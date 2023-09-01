fc2cc755d69d7d5a643bf260c19d29ab9e609019
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-02 00:09:01 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,13 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
 
-for i in range(t):
-    k = int(input())        # 층
-    n = int(input())        # 호
-    people = list(range(1, n+1))     # 0층
+for _ in range(t):
+    # k층에 n호
+    k = int(input())
+    n = int(input())
+    # 0층 리스트
+    people = list(range(1, n+1))
 
-    for x in range(k):
-        new = []
-        for y in range(n):
-            new.append(sum(people[:y+1]))   # 아래층의 1~n호 까지의 합
-        people = new.copy()
-    print(people[-1])       # K층 n호
+    for i in range(k):  # 층 수 만큼 반복
+        for j in range(1, n):  # 1 ~ n-1까지 (인덱스로 사용)
+            people[j] += people[j - 1]  # 층별 각 호실의 사람 수를 변경
+    print(people[-1])  # 가장 마지막 수 출력
