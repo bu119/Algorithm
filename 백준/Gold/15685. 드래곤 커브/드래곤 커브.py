467c3bb2abb9d4b8108071401dc655171b805e8a
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-06 17:05:40 +0900

    [Gold IV] Title: 드래곤 커브, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -1,31 +1,28 @@
-# 0세대는 주어진 선
-# 1세대는 0세대를 90도 회전후 연결한 2개로 이루어진 선
-# 2세대는 1세대를 90도 회전후 연결한 4개로 이루어진 선
-
 # 기존 선을 90도 회전하는 위치 저장 함수
-def move_90(last_i, last_j):
+def move_90(li, lj):
     global move_d
     # 시작점을 마지막 지점으로 설정하고
-    # 처음 이동 방향에 - 1을 해서 다시 이동하면 된다.
-    # -1 하면 시계 방향이 된다. (아니면 이동 방향을 저장)
+    # 마지막 지점의 방향부터 + 1을 해서 방향으로 추가해주면 된다.
+    # 마지막 지점 부터 반시계 방향으로 이동하고있다.
+    # +1 하면 반시계 방향이 된다. (이동 방향을 저장)
 
     m = len(move_d)
-    for z in range(m-1,-1,-1):
+    for z in range(m-1, -1, -1):
         # k값 0, 1, 2, 3으로 나오게 변경
-        md = (move_d[z]+1) % 4
+        k = (move_d[z] + 1) % 4
         # 변경된 위치값
-        ni = last_i + di[md]
-        nj = last_j + dj[md]
+        ni = li + di[k]
+        nj = lj + dj[k]
         if 0 <= ni <= 100 and 0 <= nj <= 100:
             # 방문 체크
             board[ni][nj] = 1
-            move_d.append(md)
-            last_i = ni
-            last_j = nj
+            move_d.append(k)
+            li = ni
+            lj = nj
         else:
             break
 
-    return last_i, last_j
+    return li, lj
 
 
 # 정사각형인지 판별하는 함수
@@ -37,7 +34,6 @@ def find_square(r, c):
             pass
         else:
             return False
-
     return True
 
 
@@ -68,9 +64,7 @@ for _ in range(n):
         # 연결한 위치와 방향을 추가해주는 함수
         last_i, last_j = move_90(last_i, last_j)
 
-        # 시행 횟수 감소
-        g -= 1
-
+# 정사각형 개수 찾기
 ans = 0
 for r in range(101):
     for c in range(101):
