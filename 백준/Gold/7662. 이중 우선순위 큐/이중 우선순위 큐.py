d6e9487b7c188754d01f8cf0e92a12b0c4a9bb39
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-10 23:55:54 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2468 ms, Memory: 308080 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,46 +1,63 @@
 import sys, heapq
 input = sys.stdin.readline
 
+# 중복 제거
+def de_duplication(heap):
+    global min_heap, max_heap
+
+    if heap == 'max_heap':
+        # max_heap에 값이 있고 -max_heap[0]이 삭제된 값이면
+        while max_heap and visited[-max_heap[0]] < 1:
+            heapq.heappop(max_heap)
+
+    else:
+        # min_heap에 값이 있고 min_heap[0]이 삭제된 값이면
+        while min_heap and visited[min_heap[0]] < 1:
+            heapq.heappop(min_heap)
+
 
 t = int(input())
 for _ in range(t):
     k = int(input())
     max_heap = []
     min_heap = []
-    # 해당 숫자가 존재하는지 여부 저장 (삭제시 저장)
-    visited = set()
+    # 값의 추가, 삭제 기록
+    visited = {}
     for i in range(k):
         operation, n = input().split()
         n = int(n)
         
         if operation == 'I':
-            heapq.heappush(min_heap, (n, i))
-            heapq.heappush(max_heap, (-n, i))
-        else:
-            if n == 1:
-                # max_heap있고 max_heap[0]이 삭제된 값이면
-                while max_heap and max_heap[0][1] in visited:
-                    heapq.heappop(max_heap)
-                # max_heap에 값이 있으면
-                if max_heap:
-                    _, idx = heapq.heappop(max_heap)
-                    visited.add(idx)
+            # 이미 값이 존재하면 개수만 올려줌
+            if visited.get(n):
+                visited[n] += 1
             else:
-                # min_heap있고 min_heap[0]이 삭제된 값이면
-                while min_heap and min_heap[0][1] in visited:
-                    heapq.heappop(min_heap)
-                # min_heap에 값이 있으면
+                heapq.heappush(min_heap, n)
+                heapq.heappush(max_heap, -n)
+                visited[n] = 1
+
+        else:
+            if not max_heap or not min_heap:
+                continue
+
+            if n == 1:
+                # 중복 제거
+                de_duplication('max_heap')
+                # 최댓값 삭제
+                if max_heap:
+                    visited[-max_heap[0]] -= 1
+
+            else:
+                # 중복 제거
+                de_duplication('min_heap')
+                # 최솟값 삭제
                 if min_heap:
-                    _, idx = heapq.heappop(min_heap)
-                    visited.add(idx)
+                    visited[min_heap[0]] -= 1
 
-    while max_heap and max_heap[0][1] in visited:
-        heapq.heappop(max_heap)
-
-    while min_heap and min_heap[0][1] in visited:
-        heapq.heappop(min_heap)
+    de_duplication('max_heap')
+    de_duplication('min_heap')
 
     if min_heap and max_heap:
-        print(-max_heap[0][0], min_heap[0][0])
+        print(-max_heap[0], min_heap[0])
     else:
         print('EMPTY')
