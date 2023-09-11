9de3b804aeed4cf31cf46cc03faa258a6d0f6225
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-11 15:26:19 +0900

    [Gold I] Title: 낚시왕, Time: 264 ms, Memory: 127052 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -2,32 +2,41 @@ import sys
 input = sys.stdin.readline
 
 def move_shark():
-    ocean = [[[] for _ in range(c)] for _ in range(r)]
+    # 이동한 상어 저장
+    ocean = [[False]*c for _ in range(r)]
     # 전체 상어 이동
-    for row in range(r):
-        for col in range(c):
-            if sea[row][col]:
-                s, d, z = sea[row][col][0]
-                x = row
-                y = col
-                go = 0
-                # 상어 속도 만큼 이동
-                while go < s:
-                    nx = x + di[d]
-                    ny = y + dj[d]
-                    # 맵 내부에서 이동하는 경우
-                    if 0 <= nx < r and 0 <= ny < c:
-                        x, y = nx, ny
-                        go += 1
-                    # 벽과 충돌하는 경우, 방향전환
+    for x in range(r):
+        for y in range(c):
+            if sea[x][y]:
+                s, d, z = sea[x][y]
+                nx = x + di[d] * s
+                ny = y + dj[d] * s
+
+                # 위아래
+                if d == 1 or d == 2:
+                    quota = nx // (r-1)
+                    remainder = nx % (r-1)
+                    # 같은 방향
+                    if quota % 2 == 0:
+                        nx = remainder
+                    # 방향 전환
                     else:
                         d = boundary[d]
-                        continue
-
+                        nx = (r - 1) - remainder
+                # 좌우
+                else:
+                    quota = ny // (c-1)
+                    remainder = ny % (c-1)
+                    # 같은 방향
+                    if quota % 2 == 0:
+                        ny = remainder
+                    # 방향 전환
+                    else:
+                        d = boundary[d]
+                        ny = (c - 1) - remainder
                 # 큰 상어만 남기기
-                if not ocean[x][y] or ocean[x][y][0][2] < z:
-                    ocean[x][y] = [(s, d, z)]
-
+                if not ocean[nx][ny] or ocean[nx][ny][2] < z:
+                    ocean[nx][ny] = (s, d, z)
     return ocean
 
 
@@ -37,29 +46,30 @@ def catch_shark(j):
     for i in range(r):
         if sea[i][j]:
             # 상어를 잡는다.
-            s, d, z = sea[i][j].pop()
+            s, d, z = sea[i][j]
+            sea[i][j] = False
             return z
     return 0
 
 
 # 격자판의 크기 R, C와 상어의 수 M
 r, c, m = map(int, input().split())
-sea = [[[] for _ in range(c)] for _ in range(r)]
+sea = [[False]*c for _ in range(r)]
 
-# d가 1인 경우는 위, 2인 경우는 아래, 3인 경우는 오른쪽, 4인 경우는 왼쪽을 의미한다.
+# d가 1인 경우는 위, 2인 경우는 아래, 3인 경우는 오른쪽, 4인 경우는 왼쪽
 di = [0, -1, 1, 0, 0]
 dj = [0, 0, 0, 1, -1]
 boundary = {1:2, 2:1, 3:4, 4:3}
 
 for _ in range(m):
-    # (rr, cc)는 상어의 위치, s는 속력, d는 이동 방향, z는 크기이다.
+    # (r, c)는 상어의 위치, s는 속력, d는 이동 방향, z는 크기
     rr, cc, s, d, z = map(int, input().split())
-    sea[rr-1][cc-1].append((s, d, z))
+    sea[rr-1][cc-1] = (s, d, z)
 
 fishing = 0
 for j in range(c):
     # 낚시
     fishing += catch_shark(j)
-    # 상어가 이동한다.
+    # 상어 이동
     sea = move_shark()
 print(fishing)
