d9cdc6ae705c29eefd441313435e9bce13f2cedb
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-21 23:57:26 +0900

    [Silver II] Title: 좌표 압축, Time: 1292 ms, Memory: 302876 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -2,11 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-coordinate = list(map(int, input().split()))
-arr = sorted(set(coordinate))
+x_arr = list(map(int, input().split()))
+# 중복 제거 후 오름차순 정렬
+sort_arr = sorted(set(x_arr))
+# 작은 수 개수 저장
 cnt = {}
-for i in range(len(arr)):
-    cnt[arr[i]] = i
+# 인덱스 = 작은수 개수
+for i in range(len(sort_arr)):
+    cnt[sort_arr[i]] = i
 
-for j in coordinate:
-    print(cnt[j], end = ' ')
+for x in x_arr:
+    print(cnt[x], end=' ')
