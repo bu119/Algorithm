9e04a45352a4a9d1e670a9b8ecb72cb4b9eda5a6
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-25 12:12:23 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 68 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,3 +1,12 @@
+# 직사각형 보드에 빨간 구슬과 파란 구슬을 하나씩 넣은 다음, 빨간 구슬을 구멍을 통해 빼내는 게임
+# 가장 바깥 행과 열은 모두 막혀져 있고, 보드에는 구멍이 하나 있다.
+# 게임의 목표는 빨간 구슬을 구멍을 통해서 빼내는 것이다. 이때, 파란 구슬이 구멍에 들어가면 안 된다.
+# 중력을 이용해서 이리 저리 굴려야 한다.
+# 왼쪽으로 기울이기, 오른쪽으로 기울이기, 위쪽으로 기울이기, 아래쪽으로 기울이기와 같은 네 가지 동작이 가능하다.
+# 공은 동시에 움직인다.
+# 빨간 구슬이 구멍에 빠지면 성공이지만, 파란 구슬이 구멍에 빠지면 실패이다.
+# 빨간 구슬과 파란 구슬이 동시에 구멍에 빠져도 실패이다.
+# 최소 몇 번 만에 빨간 구슬을 구멍을 통해 빼낼 수 있는지
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -6,8 +15,8 @@ def bfs(rx, ry, bx, by):
     deq = deque()
     deq.append((rx, ry, bx, by))
     # 방문체크
-    visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
-    visited[rx][ry][bx][by] = True
+    visited = set()
+    visited.add((rx, ry, bx, by))
     cnt = 0
     while deq:
         # 같은 이동 횟수끼리 탐색
@@ -55,9 +64,9 @@ def bfs(rx, ry, bx, by):
                         nbx -= dx[k]
                         nby -= dy[k]
                 # 방문 체크
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
+                if (nrx, nry, nbx, nby) not in visited:
                     deq.append((nrx, nry, nbx, nby))
+                    visited.add((nrx, nry, nbx, nby))
         cnt += 1
     return -1
 
@@ -67,6 +76,7 @@ board = []
 for i in range(n):
     arr = list(input())
     board.append(arr)
+    # 빨간 구슬, 파란 구슬 위치 저장
     for j in range(m):
         if arr[j] == 'R':
             ri, rj = i, j
