05ad74365d47fcf108304dc9ddebcde6f8ba191e
bu119 <109335452+bu119@users.noreply.github.com>
2023-09-29 00:42:42 +0900

    [Gold III] Title: 소문난 칠공주, Time: 648 ms, Memory: 124568 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,46 +1,73 @@
 from collections import deque
-from itertools import combinations
+import sys
+input = sys.stdin.readline
 
+# 7명 선택
+def dfs(idx, yCnt, memCnt):
+    global cnt, members
 
-def bfs(x, y):
-    total = 1
+    # 임도‘연’파가 4이상이면 이다‘솜’파가 4이상 안됨
+    if yCnt >= 4:
+        return
+
+    if memCnt == 7:
+        if bfs(members):
+            cnt += 1
+        return
+
+    for k in range(idx, 25):
+        # 25번 까지 중 행은 번호를 5로 나눈 몫
+        ni = k // 5
+        # 25번 까지 중 열은 번호를 5로 나눈 나머지
+        nj = k % 5
+        # 임도‘연’파 변화 저장
+        new = yCnt
+        if students[ni][nj] == 'Y':
+            new += 1
+        # 해당 위치를 멤버로 추가
+        members.append((ni, nj))
+        dfs(k+1, new, memCnt+1)
+        # 해당 위치를 제거 (멤버X)
+        members.pop()
+
+        
+# 7명이 인접해 있는지 확인        
+def bfs(members):
     deq = deque()
-    deq.append((x, y))
-    # 방문 위치 제거
-    visited[i][j] = 0
+    # 7공주 방문 체크
+    visited = [[1] * 5 for _ in range(5)]
+
+    # 첫 사람부터 탐색 시작
+    total = 1
+    deq.append(members[0])
+    # 방문 위치 제거하고 저장
+    for x, y in members[1:]:
+        visited[x][y] = 0
 
     while deq:
         x, y = deq.popleft()
 
         for k in range(4):
-            ni = x + di[k]
-            nj = y + dj[k]
-            if 0 <= ni < 5 and 0 <= nj < 5 and visited[ni][nj] == 1:
-                # 방문한 위치 제거
-                visited[ni][nj] = 0
-                deq.append((ni,nj))
+            nx = x + di[k]
+            ny = y + dj[k]
+            if 0 <= nx < 5 and 0 <= ny < 5 and visited[nx][ny] == 0:
+                # 방문 체크
+                visited[nx][ny] = 1
+                deq.append((nx,ny))
                 total += 1
+    # 7명 모두 인접
     if total == 7:
-        return 1
-    return 0
+        return True
+    return False
 
 
 students = [input() for _ in range(5)]
 di = [0,1,0,-1]
 dj = [1,0,-1,0]
+# 7공주 경우의 수 저장
 cnt = 0
-for members in combinations(range(25), 7):
-    # 7공주 체크
-    visited = [[0] * 5 for _ in range(5)]
-    sCnt = 0
-    for num in members:
-        i = num // 5
-        j = num % 5
-        visited[i][j] = 1
-        if students[i][j] == 'S':
-            sCnt += 1
-
-    if sCnt >= 4 and bfs(i, j):
-        cnt += 1
+# 7공주 멤버 저장
+members = []
+dfs(0, 0, 0)
 
 print(cnt)
