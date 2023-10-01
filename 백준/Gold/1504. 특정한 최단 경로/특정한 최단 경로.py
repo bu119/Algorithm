810cb5ad188a49ad761a8d131036373be05ade92
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-02 00:01:00 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 408 ms, Memory: 64392 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -5,7 +5,6 @@ input = sys.stdin.readline
 # start에서 각 점의 최단거리
 def dijkstra(start):
     visited = [INF]*(n+1)
-
     heap = []
     # 최단거리, 시작 위치 저장
     heapq.heappush(heap, (0, start))
@@ -19,33 +18,32 @@ def dijkstra(start):
         for w, nextDist in graph[v]:
             totalDist = dist + nextDist
             if totalDist < visited[w]:
-                visited[w] = totalDist
                 heapq.heappush(heap, (totalDist, w))
+                visited[w] = totalDist
 
     return visited
 
 
-# 방향성이 없는 그래프
 n, e = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
     graph[b].append((a, c))
-
+    
 v1, v2 = map(int, input().split())
 
 # 최댓값
-INF = 200000001
+INF = int(1e9)
 
 # 출발점이 다른 3가지 경우
-start = dijkstra(1)
+start1 = dijkstra(1)
 startV1 = dijkstra(v1)
 startV2 = dijkstra(v2)
 # 1, v1, v2, n
-case1 = start[v1] + startV1[v2] + startV2[n]
+case1 = start1[v1] + startV1[v2] + startV2[n]
 # 1, v2, v1, n
-case2 = start[v2] + startV2[v1] + startV1[n]
+case2 = start1[v2] + startV2[v1] + startV1[n]
 
 ans = min(case1, case2)
 if ans < INF:
