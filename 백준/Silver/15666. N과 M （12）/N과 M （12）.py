62a29c788136fcb6442873189a445dd70f9a631e
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-05 23:35:08 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory:	31256 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -3,5 +3,6 @@ from itertools import combinations_with_replacement
 n, m = map(int, input().split())
 arr = sorted(map(int, input().split()))
 ans = sorted(set(combinations_with_replacement(arr, m)))
+
 for i in ans:
-    print(*i)
+    print(' '.join(map(str, i)))
