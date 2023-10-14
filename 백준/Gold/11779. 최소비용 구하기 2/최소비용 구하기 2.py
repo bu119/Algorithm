89c63513543f8068884e81336a41da42ab9939f5
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-15 00:18:14 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 284 ms, Memory: 57396 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,5 +1,6 @@
 import sys, heapq
 input = sys.stdin.readline
+
 def dijkstra(start, end):
     global route
     # 비용 저장
@@ -8,24 +9,22 @@ def dijkstra(start, end):
     heap = [(0, start)]
     # 최소 비용 저장
     visited[start] = 0
-    # 경로
-    route[start] = [start]
 
     while heap:
-        currCost, city = heapq.heappop(heap)
+        curCost, curCity = heapq.heappop(heap)
 
         # 저장된 비용보다 크면 탐색 안함
-        if visited[city] < currCost:
+        if visited[curCity] < curCost:
             continue
 
-        for nextCity, moveCost in graph[city]:
-            totalCost = currCost + moveCost
+        for nextCity, moveCost in graph[curCity]:
+            totalCost = curCost + moveCost
             # 비용이 적으면 갱신
             if totalCost < visited[nextCity]:
                 # 최소 비용 저장
                 visited[nextCity] = totalCost
-                # 경로 갱신
-                route[nextCity] = route[city] + [nextCity]
+                # 경로 갱신 (이전 도시 저장)
+                route[nextCity] = curCity
 
                 heapq.heappush(heap, (totalCost, nextCity))
 
@@ -35,13 +34,26 @@ def dijkstra(start, end):
 n = int(input())
 m = int(input())
 graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
     s, e, c = map(int, input().split())
     graph[s].append((e, c))
 
 start, end = map(int, input().split())
+# 도시 이동 경로 저장
+route = [0] * (n+1)
 
-route = [[] for _ in range(n+1)]
+# 출력
 print(dijkstra(start, end))
-print(len(route[end]))
-print(*route[end])
+
+# 도시 이동 경로
+preCity = end
+cityRoute = [str(end)]
+# 출발점은 0이 나온다.
+while route[preCity]:
+    preCity = route[preCity]
+    cityRoute.append(str(preCity))
+    
+# 출력
+print(len(cityRoute))
+print(' '.join(reversed(cityRoute)))
