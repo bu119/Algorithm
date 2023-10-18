154096e091c4094ae68f62d6ae5f622d25d9ee0e
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-19 02:33:52 +0900

    [Gold IV] Title: 서강그라운드, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,28 +1,47 @@
+import heapq
+
+def dijkstra(start):
+    # 최소 거리, 위치
+    heap = [(0, start)]
+    visited[start] = 0
+    # 범위 내 지역 저장
+    getItem.add(start)
+
+    while heap:
+        dist, curr = heapq.heappop(heap)
+
+        if visited[curr] < dist:
+            continue
+
+        for next, d in graph[curr]:
+            sumDist = dist + d
+            if sumDist < visited[next]:
+                visited[next] = sumDist
+                getItem.add(next)
+                heapq.heappush(heap,(sumDist, next))
+
+                
+# 각 지점에서 최소거리를 구한다. (다익스트라)
+# 최소거리가 m이하인 지점의 아이템을 더한다.
+# 각 점의 아이템을 더한 값을 비교하여 최소값을 찾는다.
 n, m, r = map(int, input().split())
 items = [0] + list(map(int, input().split()))
-graph = [[150000] * (n + 1) for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 for _ in range(r):
     a, b, l = map(int, input().split())
-    graph[a][b] = l
-    graph[b][a] = l
-
-for i in range(n + 1):
-    graph[i][i] = 0
-
-# 점화식에 따라 플로이드 워셜 알고리즘을 수행
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    graph[a].append((b, l))
+    graph[b].append((a, l))
 
 ans = 0
-# 저장한 최댓값을 출력
-for x in range(1, n + 1):
-    ssum = 0
-    for y in range(1, n + 1):
-        if graph[x][y] <= m:
-            ssum += items[y]
-            
-    ans = max(ans, ssum)
-    
+for i in range(1,n+1):
+    # 범위 내 지역
+    getItem = set()
+    # 거리 저장
+    visited = [m+1]*(n+1)
+    dijkstra(i)
+    # 아이템 최대 개수
+    totalItems = 0
+    for j in getItem:
+        totalItems += items[j]
+    ans = max(ans, totalItems)
 print(ans)
