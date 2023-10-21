512c21961603bc0219046f3c751f4b2a9e4905a7
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-21 23:00:04 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.64 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s) {
+
+        String[] arr = s.split(" ");
+        
+        int maxV = Integer.parseInt(arr[0]);
+        int minV = Integer.parseInt(arr[0]);
+        
+        for (int i=1; i < arr.length;i++){
+            int num = Integer.parseInt(arr[i]);
+            
+            if (maxV < num) {
+                maxV = num;
+            }
+            
+            if (minV > num) {
+                minV = num;
+            }
+            
+        }
+        
+        return  Integer.toString(minV) + " " + Integer.toString(maxV);
+    }
+}
