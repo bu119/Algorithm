2437ba7f4bc24812eeb66642a2b05265f8c6ce18
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-22 03:21:30 +0900

    [level 2] Title: 올바른 괄호, Time: 19.42 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+                
+            } else{
+                if (stack.isEmpty() || stack.pop() == ')') {
+                    return false; 
+                }
+            }
+        }
+        if (!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
