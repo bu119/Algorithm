a875c4d1dc015e81bf85236066ea57475024db24
bu119 <109335452+bu119@users.noreply.github.com>
2023-10-28 22:57:11 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 104 ms, Memory: 36696 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,29 +1,35 @@
 from collections import deque
 
-def path(x):
-    arr = []
-    temp = x
-    for _ in range(dist[x]+1):
-        arr.append(temp)
-        temp = move[temp]
-    print(' '.join(map(str, arr[::-1])))
+def bfs(v):
+    deq = deque()
+    deq.append((v, 0))
+    # visited[v] = v
+    while deq:
+        v, moveCnt = deq.popleft()
 
-def bfs():
-    q = deque()
-    q.append(N)
-    while q:
-        x = q.popleft()
-        if x == K:
-            print(dist[x])
-            path(x)
-            return x
-        for i in (x+1, x-1, 2*x):
-            if 0<=i<=100000 and dist[i]==0:
-                q.append(i)
-                dist[i] = dist[x] + 1
-                move[i] = x
+        if v == k:
+            return moveCnt
 
-N, K = map(int, input().split())
-dist = [0]*100001
-move = [0]*100001
-bfs()
+        for w in [v - 1, v + 1, v * 2]:
+            if 0 <= w <= 100000 and visited[w] == -1:
+                deq.append((w, moveCnt+1))
+                visited[w] = v
+
+
+n, k = map(int, input().split())
+if n > k:
+    print(n-k)
+    print(*range(n, k-1, -1))
+
+else:
+    visited = [-1] * 100001
+    cnt = bfs(n)
+    print(cnt)
+
+    # 경로 찾기
+    route = []
+    posi = k
+    for _ in range(cnt + 1):
+        route.append(posi)
+        posi = visited[posi]
+    print(*reversed(route))
