b4bebe0b36422abf040b0e7ba204b1afec76195b
bu119 <109335452+bu119@users.noreply.github.com>
2023-11-14 21:39:39 +0900

    [Gold V] Title: LCS, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -2,12 +2,13 @@ string1 = input()
 string2 = input()
 n = len(string1)
 m = len(string2)
-lcs = [[0]*(m+1) for _ in range(n+1)]
+lcs = [0]*m
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if string1[i-1] == string2[j-1]:
-            lcs[i][j] = lcs[i - 1][j - 1] + 1
-        else:
-            lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
-print(lcs[n][m])
+for i in range(n):
+    cnt = 0
+    for j in range(m):
+        if cnt < lcs[j]:
+            cnt = lcs[j]
+        elif string1[i] == string2[j]:
+            lcs[j] = cnt + 1
+print(max(lcs))
