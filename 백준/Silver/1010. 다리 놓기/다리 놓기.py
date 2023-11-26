00c98b7659b61f0e8db5274b6907e1078e19b1a4
bu119 <109335452+bu119@users.noreply.github.com>
2023-11-26 18:32:19 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,10 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 # x개 중에 y개 뽑기 (순서 상관x)
 def com_cnt(x, y):
     numerator = 1
     denominator = 1
     for i in range(y):
         numerator *= (x-i)
-        denominator *=(y-i)
+        denominator *=(i+1)
     return numerator//denominator
 
 
