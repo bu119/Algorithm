455cea57a296a88961626f929b767da24a333910
bu119 <109335452+bu119@users.noreply.github.com>
2023-12-14 15:17:12 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
@@ -20,8 +20,8 @@ def union(a,b):
 n, m = map(int, input().split())
 univ = input().split()
 parent = list(range(n+1))
-# 모든 학교 연결 여부 체크
-visited = set()
+# 연결된 길 세기
+road_cnt = 0
 ans = 0
 edges = []
 for _ in range(m):
@@ -29,21 +29,16 @@ for _ in range(m):
     # 다른 성별 대학교 길만 저장
     if univ[u-1] != univ[v-1]:
         edges.append((d, u, v))
-        # 학교 방문 체크
-        visited.add(u)
-        visited.add(v)
-
+# 거리순으로 정렬
+edges.sort()
+# 최단 거리 계산
+for cost, u, v in edges:
+    if find(u) != find(v):
+        union(u, v)
+        ans += cost
+        road_cnt += 1
 # 모든 학교 연결 여부
-# 불가
-if len(visited) != n:
-    ans = -1
-# 가능
+if road_cnt == n-1:
+    print(ans)
 else:
-    # 거리순으로 정렬
-    edges.sort()
-    # 최단 거리 계산
-    for cost, u, v in edges:
-        if find(u) != find(v):
-            union(u, v)
-            ans += cost
-print(ans)
+    print(-1)
