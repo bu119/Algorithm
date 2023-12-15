2640001cdf673042c530c9d652c843f01e08d52a
bu119 <109335452+bu119@users.noreply.github.com>
2023-12-15 23:51:03 +0900

    [Gold II] Title: 전기가 부족해, Time: 300 ms, Memory: 45572 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
@@ -9,11 +9,7 @@ def find(x):
 def union(a, b):
     a = find(a)
     b = find(b)
-    if a in power_station:
-        parent[b] = a
-    elif b in power_station:
-        parent[a] = b
-    elif a < b:
+    if a < b:
         parent[b] = a
     else:
         parent[a] = b
@@ -22,24 +18,28 @@ def union(a, b):
 n, m, k = map(int, input().split())
 power_station = list(map(int, input().split()))
 parent = list(range(n+1))
+# 도시에는 발전소가 반드시 하나만 연결되야하므로
+# 발전소의 부모는 다 0으로 초기화
+for i in power_station:
+    parent[i] = 0
+
 ans = 0
 edges = []
 for _ in range(m):
     u, v, w = map(int, input().split())
-    if u in power_station and v in power_station:
+    # 발전소에서 발전소로 가는 길
+    if parent[u] == parent[v]:
         continue
     edges.append((w, u, v))
+    
 edges.sort()
+# 경로의 개수 = 전체 노드 - 발전소 노드
+road_cnt = n - len(power_station)
 
 for cost, u, v in edges:
-    pu = find(u)
-    pv = find(v)
-    if pu in power_station and pv in power_station:
-        continue
-    if pu != pv:
+    if find(u) != find(v):
         union(u, v)
         ans += cost
+        road_cnt -= 1
 
-    if len(set(parent[1:])) == len(power_station):
-        break
 print(ans)
