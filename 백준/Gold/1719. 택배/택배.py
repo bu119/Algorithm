f11b980808bea69bb83c74017094625fefec1b24
bu119 <109335452+bu119@users.noreply.github.com>
2024-01-07 12:13:58 +0900

    [Gold III] Title: 택배, Time: 296 ms, Memory: 111664 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -18,7 +18,11 @@ for x in range(1, n+1):
 
 for k in range(1, n+1):
     for i in range(1, n+1):
+        if i == k:
+            continue
         for j in range(1, n+1):
+            if j == k or i == j:
+                continue
             if dist[i][k] + dist[k][j] < dist[i][j]:
                 dist[i][j] = dist[i][k] + dist[k][j]
                 # 첫번째 집하장 저장
