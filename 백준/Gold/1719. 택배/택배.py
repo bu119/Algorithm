49b5b284eacc806a72670ee9bc5ac441253e11d0
bu119 <109335452+bu119@users.noreply.github.com>
2024-01-07 12:12:42 +0900

    [Gold III] Title: 택배, Time: 300 ms, Memory: 111664 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,37 +1,28 @@
-import sys, heapq
+import sys
 input = sys.stdin.readline
 
-def dijkstra(x):
-    dist = [2000000001] * (n + 1)
-    # 첫번째 집하장 저장
-    firstPath = ['-']*(n+1)
-    dist[x] = 0
-    # 시간, 현재 위치
-    heap = [(0, x)]
-    while heap:
-        currTime, curr = heapq.heappop(heap)
-
-        if dist[curr] < currTime:
-            continue
-
-        for next, nextTime in graph[curr]:
-            totalT = currTime + nextTime
-            if totalT < dist[next]:
-                dist[next] = totalT
-                heapq.heappush(heap, (totalT, next))
-                if firstPath[curr] == '-':
-                    firstPath[next] = next
-                else:
-                    firstPath[next] = firstPath[curr]
-    return firstPath[1:]
-
-
 n, m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+dist = [[2000000001]*(n+1) for _ in range(n+1)]
+# 첫번째 집하장 저장
+firstPath = [['-']*n for _ in range(n)]
+
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
+    dist[a][b] = c
+    dist[b][a] = c
+    firstPath[a-1][b-1] = b
+    firstPath[b-1][a-1] = a
 
-for i in range(1, n+1):
-    print(*dijkstra(i))
+for x in range(1, n+1):
+    dist[x][x] = 0
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if dist[i][k] + dist[k][j] < dist[i][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
+                # 첫번째 집하장 저장
+                firstPath[i-1][j-1] = firstPath[i-1][k-1]
+
+for i in range(n):
+    print(*firstPath[i])
