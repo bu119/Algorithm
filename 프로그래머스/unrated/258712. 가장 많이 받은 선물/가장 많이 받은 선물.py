aa892a27ee44b2241cc8ed9cfb6aea12aebe82b9
bu119 <109335452+bu119@users.noreply.github.com>
2024-01-12 23:27:33 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 1.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,7 +1,6 @@
 def solution(friends, gifts):
     n = len(friends)
-    m = len(gifts)
-    # {이름: 인덱스 ...}의 딕셔너리 형태로 저장
+    # {이름: 인덱스 ...} 형태로 저장
     friendsIdx = dict()
     for i in range(n):
         friendsIdx[friends[i]] = i
@@ -10,25 +9,26 @@ def solution(friends, gifts):
     movingInfo = [[0]*n for _ in range(n)]
     # 선물 지수 저장
     giftIndex = [0]*n
-    for j in range(m):
+    for gift in gifts:
         # A: 준 사람, B: 받은 사람
-        A, B = gifts[j].split()
+        A, B = gift.split()
         # 행: 준 사람, 열: 받은 사람
         movingInfo[friendsIdx[A]][friendsIdx[B]] += 1
-        # 준 사람 선물 지수 변동 (한번에 변경)
+        # 선물 지수 변동
+        # 준 사람 
         giftIndex[friendsIdx[A]] += 1
-        # 받은 사람 선물 지수 변동
+        # 받은 사람
         giftIndex[friendsIdx[B]] -= 1
 
     # 다음 달에 받는 선물 수 저장
     getNextMonth = [0]*n
     for a in range(n-1):
         for b in range(a+1, n):
-            # a가 b에게 주다.
+            # a가 b에게 줌
             giveToB = movingInfo[a][b]
-            # b가 a에게 주다.
+            # b가 a에게 줌
             giveToA = movingInfo[b][a]
-            # 두 사람이 선물을 주고받은 기록이 
+            # 두 사람의 선물 기록이 
             # 하나도 없거나 같다면
             if giveToB == giveToA or (not giveToB and not giveToA):
                 # 선물 지수 큰 사람 + 1
