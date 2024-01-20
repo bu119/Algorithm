9de00eec5fa2d00f90b133649590b7ea210727b6
bu119 <109335452+bu119@users.noreply.github.com>
2024-01-20 19:38:38 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,12 +1,4 @@
 s = input()
-s0 = s.split('1')
-s1 = s.split('0')
-cnt0 = 0
-cnt1 = 0
-for i in s0:
-    if i != '':
-        cnt0+=1
-for j in s1:
-    if j != '':
-        cnt1 += 1
-print(min(cnt0, cnt1))
+s0 = s.replace('1', ' ').split()
+s1 = s.replace('0', ' ').split()
+print(min(len(s0), len(s1)))
