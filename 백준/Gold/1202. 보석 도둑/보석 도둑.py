09928bc3004713f73d51cc50655a6afef985c26a
bu119 <109335452+bu119@users.noreply.github.com>
2024-01-24 00:38:57 +0900

    [Gold II] Title: 보석 도둑, Time: 552 ms, Memory: 198752 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -5,7 +5,6 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 
 # 각 보석 무게 M, 가격 V
-# 큰 순으로 정렬 - 뒤에서 부터 pop하려고
 jewels = dict()
 for _ in range(n):
     m, v = map(int, input().split())
@@ -14,10 +13,11 @@ for _ in range(n):
     else:
         jewels[m] = [v]
 # 보석 무게만 저장
+# 무게 큰 순으로 정렬 - 뒤에서 부터 pop하려고
 jewelsW = sorted(jewels, reverse=True)
 
 # 각 가방에 담을 수 있는 최대 무게 C
-bagWeight = [int(input()) for i in range(k)]
+bagWeight = [int(input()) for _ in range(k)]
 # 가방 작은 순
 bagWeight.sort()
 
@@ -25,14 +25,14 @@ stealPrice = 0
 # 현재 가방에 담을 수 있는 보석 가격 저장
 steal = []
 # 작은 가방부터 탐색하여 가능한 보석 가격 모두 저장
-for j in range(k):
-    # j번째 가방에 들어가는 보석이면
-    while jewelsW and jewelsW[-1] <= bagWeight[j]:
+for i in range(k):
+    # i번째 가방에 들어가는 보석이면
+    while jewelsW and jewelsW[-1] <= bagWeight[i]:
         jw = jewelsW.pop()
         # 보석 가격 저장
         for jp in jewels[jw]:
             heapq.heappush(steal, -jp)
-    # j번째 가방에 안 들어가거나 보석이 더 이상 없으면
+    # i번째 가방에 안 들어 가거나 보석이 더 이상 없으면
     # 저장된 보석 중 제일 큰 가격 저장
     if steal:
         stealPrice -= heapq.heappop(steal)
