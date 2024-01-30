367b8b648b7a0627dd4076680bf151ae8ca94ebe
bu119 <109335452+bu119@users.noreply.github.com>
2024-01-31 01:56:58 +0900

    [Gold IV] Title: 불, Time: 636 ms, Memory: 226532 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,42 +1,30 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
 def bfs(x, y):
-    global fire, building
-
-    visited = [[0]*w for _ in range(h)]
+    global deq, visited, building
+    # 상근이 위치는 뒤에 놓기
+    deq.append((x, y))
     visited[x][y] = 1
-    stack = [(x, y)]
+    while deq:
+        x, y = deq.popleft()
 
-    while stack:
-        # 퍼진 불 위치 저장
-        newFire = []
-        # 불 먼저 이동
-        for x, y in fire:
-            for k in range(4):
-                nx = x + dx[k]
-                ny = y + dy[k]
-                if 0 <= nx < h and 0 <= ny < w and building[nx][ny] == '.':
-                    # 불 퍼짐
+        # 사람이 탈출 하면
+        if visited[x][y] > 0 and (x in {0, h-1} or y in {0, w-1}):
+            return visited[x][y]
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < h and 0 <= ny < w and building[nx][ny] == '.' and visited[nx][ny] == 0:
+                deq.append((nx, ny))
+
+                if visited[x][y] == -1:
+                    visited[nx][ny] = -1
                     building[nx][ny] = '*'
-                    newFire.append((nx, ny))
-        fire = newFire
-        
-        # 상근이 이동 위치 저장
-        newSG = []
-        # 상근이 이동
-        for x, y in stack:
-
-            if x in {0, h-1} or y in {0, w-1}:
-                return visited[x][y]
-
-            for k in range(4):
-                nx = x + dx[k]
-                ny = y + dy[k]
-                if 0 <= nx < h and 0 <= ny < w and building[nx][ny] == '.' and visited[nx][ny] == 0:
+                else:
                     visited[nx][ny] = visited[x][y] + 1
-                    newSG.append((nx, ny))
-        stack = newSG
 
     return 'IMPOSSIBLE'
 
@@ -47,13 +35,16 @@ dy = [1, 0, -1, 0]
 for _ in range(t):
     w, h = map(int, input().split())
     building = []
-    fire = []
+    deq = deque()
+    # 불, 사람, 다 방문 체크 (불 난데 사람 못가고, 갔는데 안가는 거는 똑같음)
+    visited = [[0]*w for _ in range(h)]
     for i in range(h):
         row = list(input())
         building.append(row)
         for j in range(w):
             if row[j] == "*":
-                fire.append((i, j))
+                deq.append((i, j))
+                visited[i][j] = -1
             elif row[j] == '@':
                 sgi, sgj = i, j
                 # 상근이 위치를 저장하고 빈 공간으로 변경
