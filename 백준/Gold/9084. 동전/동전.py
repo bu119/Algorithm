6291a191266b17a801372ceffd67ec4a7eaf9a10
bu119 <109335452+bu119@users.noreply.github.com>
2024-02-13 23:24:08 +0900

    [Gold V] Title: 동전, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -6,23 +6,19 @@ for _ in range(t):
     # 동전 개수
     n = int(input())
     # 동전의 각 금액
-    coins = [0] + list(map(int, input().split()))
+    coins = list(map(int, input().split()))
     # 만드는 금액
     m = int(input())
-    dp = [[0] * (m+1) for _ in range(n+1)]
-    # 각 동전으로 0을 만들 수 있는 경우는 무조건 1가지씩 존재
-    for i in range(1, n+1):
-        dp[i][0] = 1
-
+    dp = [0] * (m+1)
+    # 0을 만들 수 있는 경우의 수는 1
+    dp[0] = 1
     # 행: 동전 번호
-    for k in range(1, n+1):
-        # 열: 최대 가치
+    for k in range(n):
+        # 열: 최대 가치 (배낭 무게)
         for w in range(1, m+1):
-            # dp[2][10]: 1 ~ 2번째 동전을 사용하여 10을 만드는 경우의 수 (최대 가치를 배낭의 무게라 칭하겠다.)
-            # 이전 동전까지 w를 만드는 경우의 수를 추가 (k번째 동전을 사용하지 않음)
-            dp[k][w] = dp[k-1][w]
-            # 배낭에 동전을 넣을수 있으면 k 번째 동전을 사용한 경우의 수 추가
+            # dp[10]: k번째 동전까지 사용해서 10을 만드는 경우의 수
+            # 배낭에 k번째 동전이 들어가면 k번째 동전을 뺀 무게 배낭의 경우의 수 추가
             if w - coins[k] >= 0:
-                # 동전 크기를 뺀 무게 배낭까지 만들수 있는 경우의 수를 추가
-                dp[k][w] += dp[k][w-coins[k]]
-    print(dp[n][m])
+                # 1번째, 2번째, ... 동전까지만 사용한 경우의 수를 순차 적으로 추가
+                dp[w] += dp[w-coins[k]]
+    print(dp[m])
