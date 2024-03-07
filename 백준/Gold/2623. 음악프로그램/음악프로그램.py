3a58602a1f27136b59461688a7a4629d9ce6be13
bu119 <109335452+bu119@users.noreply.github.com>
2024-03-07 23:50:41 +0900

    [Gold III] Title: 음악프로그램, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.py
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -6,21 +6,20 @@ n, m = map(int, input().split())
 indegree = [0]*(n+1)
 # 각 노드 별 나가는 간선의 위치 저장
 graph = [[] for _ in range(n+1)]
-
+# 시작 노드 찾기
+starter = set(range(1, n+1))
 for _ in range(m):
     num, *singers = list(map(int, input().split()))
-
     for i in range(num-1):
         a = singers[i]
         b = singers[i+1]
         indegree[b] += 1
         graph[a].append(b)
+        # 첫 번째가 될 수 없는 노드 제거 (discard은 원소 없어도 에러 발생 안함)
+        starter.discard(b)
 
-queue = []
-for i in range(1, n+1):
-    if indegree[i] == 0:
-        queue.append(i)
-
+# 진입 차수가 0인 노드
+queue = list(starter)
 answer = []
 while queue:
     curr = queue.pop(0)
