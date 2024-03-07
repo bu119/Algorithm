dae481de78f38deb2507e2d7e8c3712a41afa88a
bu119 <109335452+bu119@users.noreply.github.com>
2024-03-08 00:44:42 +0900

    [Gold IV] Title: 작업, Time: 192 ms, Memory: 110692 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -2,29 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-indegree = [0] * (n+1)
-graph = [[] for _ in range(n+1)]
-time = [0] * (n+1)
 dp = [0] * (n+1)
-queue = []
+
 for i in range(1, n+1):
     wt, cnt, *num = map(int, input().split())
-    time[i] = wt
-    if cnt == 0:
-        queue.append(i)
-        dp[i] = wt
-        continue
+    dp[i] = wt
     for j in num:
-        graph[j].append(i)
-        indegree[i] += 1
-
-while queue:
-    curr = queue.pop(0)
-
-    for next in graph[curr]:
-        indegree[next] -= 1
-        dp[next] = max(dp[next], dp[curr] + time[next])
-        if indegree[next] == 0:
-            queue.append(next)
-
+        dp[i] = max(dp[i], dp[j] + wt)
 print(max(dp))
