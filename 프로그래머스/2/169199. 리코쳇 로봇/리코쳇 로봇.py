0f3e1ca29ee62502b39f54d63b89242c4592f8e7
bu119 <109335452+bu119@users.noreply.github.com>
2024-03-09 00:21:58 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.97 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,4 +1,4 @@
-import heapq
+from collections import deque
 
 def solution(board):
     # 미끄러지기 (벽이나 장애물 만나면 멈춤)
@@ -10,41 +10,38 @@ def solution(board):
         return ni, nj
 
 
-    def dijkstra(x, y):
-        visited = [[maxV]*m for _ in range(n)]
-        heap = [(0, x, y)]
+    def bfs(x, y):
+        visited = [[-1]*m for _ in range(n)]
+        queue = deque()
+        queue.append((x,y))
         visited[x][y] = 0
 
-        while heap:
-            cost, x, y = heapq.heappop(heap)
+        while queue:
+            x, y = queue.popleft()
             # 목표지점에 도달하면 멈춤
             if board[x][y] == 'G':
-                return cost
-            # 이미 지난 곳이면 탐색 안함
-            if visited[x][y] < cost:
-                continue
+                return visited[x][y]
 
             for k in range(4):
                 nx, ny = move_robot(x, y, k)
-                # 위치 그대로면 탐색 안함
+                # 이동 위치가 변함없으면 탐색 안함
                 if x == nx and y == ny:
                     continue
-                # 움직이면 횟수 추가
-                newCost = cost + 1
-                if newCost < visited[nx][ny]:
-                    visited[nx][ny] = newCost
-                    heapq.heappush(heap, (newCost, nx, ny))
+                # 안 간 곳 이면 방문체크, 탐색 진행
+                if visited[nx][ny] == -1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx, ny))
         # 목표 위치에 도달할 수 없으면
         return -1
     
-    maxV = float('inf')
+
     di = [-1, 1, 0, 0]
     dj = [0, 0, -1, 1]
     n = len(board)
     m = len(board[0])
     
     start = [-1, -1]
-    
+    # 시작점 찾기
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
@@ -54,4 +51,4 @@ def solution(board):
         if start[0] != -1:
             break
 
-    return dijkstra(start[0], start[1])
+    return bfs(start[0], start[1])
