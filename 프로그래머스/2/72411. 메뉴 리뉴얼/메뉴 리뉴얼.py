3a7f21f8c355742d6d5364f7de648a2538838bdd
bu119 <109335452+bu119@users.noreply.github.com>
2024-07-10 14:04:54 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.22 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -8,22 +8,25 @@ from itertools import combinations
 
 def solution(orders, course):
     answer = []
-    for num in course:
-        # num개 구성이 가능한 후보와 개수 저장
+    for cnt in course:
+        # cnt개 구성이 가능한 후보의 개수 저장
+        # menu = {'AC': 4, 'CDE': 3, ...}
         candidates = {}
-        
+        # 손님이 주문한 단품메뉴
         for order in orders:
-            
-            if len(order) < num:
+            # 손님이 주문한 단품메뉴 개수가 cnt개 이상일 때만 탐색
+            if len(order) < cnt:
                 continue
-                
-            candidate = combinations(sorted(order), num)
-            for case in candidate:
+            # cnt개 단품메뉴의 조합들 (알파벳 오름차순으로 정렬하여 조합)
+            menu = combinations(sorted(order), cnt)
+            # 후보에 개수 추가
+            for case in menu:
                 if case in candidates:
                     candidates[case] += 1
                 else:
                     candidates[case] = 1
-        
+            
+        # 후보가 있고, 가장 많이 주문한 메뉴가 2명 이상 일 때
         if candidates and max(candidates.values()) > 1:
             maxV = max(candidates.values())
             for case in candidates:
