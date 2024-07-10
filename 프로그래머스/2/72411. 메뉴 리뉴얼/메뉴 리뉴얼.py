f9ad8bd47191f351cbad8b195929858b8c714759
bu119 <109335452+bu119@users.noreply.github.com>
2024-07-10 14:12:50 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.38 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -5,7 +5,6 @@
 
 from itertools import combinations
 
-
 def solution(orders, course):
     answer = []
     for cnt in course:
@@ -15,22 +14,24 @@ def solution(orders, course):
         # 손님이 주문한 단품메뉴
         for order in orders:
             # 손님이 주문한 단품메뉴 개수가 cnt개 이상일 때만 탐색
-            if len(order) < cnt:
-                continue
-            # cnt개 단품메뉴의 조합들 (알파벳 오름차순으로 정렬하여 조합)
-            menu = combinations(sorted(order), cnt)
-            # 후보에 개수 추가
-            for case in menu:
-                if case in candidates:
-                    candidates[case] += 1
-                else:
-                    candidates[case] = 1
+            # cnt개 단품메뉴의 조합들
+            if len(order) >= cnt:
+                # 알파벳 오름차순으로 정렬하여 조합
+                for comb in combinations(sorted(order), cnt):
+                    # 후보에 개수 추가
+                    if comb in candidates:
+                        candidates[comb] += 1
+                    else:
+                        candidates[comb] = 1
             
-        # 후보가 있고, 가장 많이 주문한 메뉴가 2명 이상 일 때
-        if candidates and max(candidates.values()) > 1:
+        # 가능한 후보가 있고
+        if candidates:
             maxV = max(candidates.values())
-            for case in candidates:
-                if maxV == candidates[case]:
-                    answer.append("".join(case))
+            # 최대 주문 메뉴의 주문 인원이 2명 이상일 때
+            if maxV > 1:
+                for menu, freq in candidates.items():
+                    # 최대 주문 수와 빈도가 같으면 배열에 저장
+                    if freq == maxV:
+                        answer.append("".join(menu))
 
     return sorted(answer)
