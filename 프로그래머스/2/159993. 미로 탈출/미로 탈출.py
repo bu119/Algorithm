da05c5c84bf6dc11e4e4264f7c5c35d93128ccf7
bu119 <109335452+bu119@users.noreply.github.com>
2024-07-17 16:59:59 +0900

    [level 2] Title: 미로 탈출, Time: 17.47 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -2,12 +2,12 @@
 # 통로들 중 한 칸에는 미로를 빠져나가는 문이 있는데, 레버를 당겨서만 열 수 있다.
 # 출발 지점에서 먼저 레버가 있는 칸으로 이동하여 레버를 당긴 후 미로를 빠져나가는 문이 있는 칸으로 이동
 # 출발 - 레버 - 문
-# dfs 2번
+
 from collections import deque
 
 def solution(maps):
     
-    # 시작점을 찾는 함수
+    # 시작 점을 찾는 함수
     def findS(board, row, col):
         for i in range(row):
             for j in range(col):
@@ -43,15 +43,17 @@ def solution(maps):
     
     di = [0, 1, 0, -1]
     dj = [1, 0, -1, 0]
-    
+    # 시작 점 찾기
     si, sj = findS(maps, n, m)
+    # 레버까지의 최단 거리와 위치 찾기
     minL, li, lj = bfs(maps, si, sj, "L")
-    
+    # 도착할 수 없으면 -1 반환
     if minL == -1:
         return -1
-
+    # 레버를 당기고
+    # 출구까지의 최단 거리와 위치 찾기
     minE, ei, ej = bfs(maps, li, lj, "E")
-
+    # 도착할 수 없으면 -1 반환
     if minE == -1:
         return -1
     
