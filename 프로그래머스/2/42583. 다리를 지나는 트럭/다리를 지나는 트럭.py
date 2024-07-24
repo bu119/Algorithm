da8cbe98b355e4b49588141c1df873907a2e29cd
bu119 <109335452+bu119@users.noreply.github.com>
2024-07-24 17:33:05 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 305.95 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -6,19 +6,22 @@ def solution(bridge_length, weight, truck_weights):
     time = 0
     # 다리 길이 만큼 베열 만들기
     bridge = [0] * bridge_length
+    # 다리에 있는 트럭 무게 저장
     bridge_weight = 0
+    
     while truck_weights:
         # 초 증가
         time += 1
         # 1.트럭 이동 (다리에서 무게 제거)
         bridge_weight -= bridge.pop(0)
-        # 2.다리 무게 여유가 없으면 대기
+        # 2.다리에 무게 여유 없으면 대기
         if bridge_weight + truck_weights[0] > weight:
             # 다리에 무게 0 넣어주기
             bridge.append(0)
             continue
-        # 무게 여유 있으면 들어가기 (다리에 무게 추가)
+        # 3.다리에 무게 여유 있으면 들어가기 (다리에 무게 추가)
         bridge_weight += truck_weights[0]
         bridge.append(truck_weights.pop(0))
-
+        
+    # 대기 트럭이 모두 다리에 올라가면 다리 무게 만큼 시간 추가 후 시간 반환
     return time + bridge_length
