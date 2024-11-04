28c2281ee674bf34cba68d3d5cc0c72845fe9d56
bu119 <109335452+bu119@users.noreply.github.com>
2024-11-04 22:01:04 +0900

    [Silver III] Title: 배열 복원하기, Time: 104 ms, Memory: 37604 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -2,15 +2,11 @@ import sys
 input = sys.stdin.readline
 
 h, w, x, y = map(int, input().split())
-arrA = [[0]*w for _ in range(h)]
-arrB = [list(map(int, input().split())) for _ in range(h+x)]
+arr = [list(map(int, input().split())) for _ in range(h+x)]
+
+for i in range(x, h):
+    for j in range(y, w):
+        arr[i][j] -= arr[i-x][j-y]
 
 for i in range(h):
-    for j in range(w):
-        if i >= x and j >= y:
-            arrA[i][j] = arrB[i][j] - arrA[i-x][j-y]
-        else:
-            arrA[i][j] = arrB[i][j]
-
-for arr in arrA:
-    print(" ".join(map(str, arr)))
+    print(" ".join(map(str, arr[i][:w])))
