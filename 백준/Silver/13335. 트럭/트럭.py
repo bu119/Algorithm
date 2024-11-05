014049665f606c46a79f057c873638b9064be388
bu119 <109335452+bu119@users.noreply.github.com>
2024-11-05 19:27:02 +0900

    [Silver I] Title: 트럭, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -2,20 +2,21 @@ n, w, l = map(int, input().split())
 trucks = list(map(int, input().split()))
 # 시간 저장
 time = 0
-# 다리 각 부분에 해당하는 무게 저장
+# 다리 각 위치에 해당하는 트럭 저장
 bridge = [0]*w
-# 다리의 트럭 무게
+# 다리 위의 전체 트럭 무게
 bridgeW = 0
 
 while trucks or bridgeW:
-    # 맨 앞 트럭 보냄
+    # 맨 앞 트럭 내보냄
     bridgeW -= bridge.pop(0)
     # 새로운 트럭이 있고 진입 했을 때 최대 하중 이하면
-    if trucks and bridgeW <= l - trucks[0]:
+    if trucks and bridgeW + trucks[0] <= l:
+        # 진입 트럭
         truck = trucks.pop(0)
         # 다리에 진입
         bridge.append(truck)
-        # 무게 추가
+        # 다리에 트럭 무게 추가
         bridgeW += truck
     else:
         # 최대 하중을 초과하면 다리에 진입 못함
