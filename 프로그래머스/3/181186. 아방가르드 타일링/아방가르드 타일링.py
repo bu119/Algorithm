de55441562ff67c4e530e932f50feb766e6e302c
bu119 <109335452+bu119@users.noreply.github.com>
2024-11-07 23:30:22 +0900

    [level 3] Title: 아방가르드 타일링, Time: 54.14 ms, Memory: 13.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -0,0 +1,13 @@
+def solution(n):
+    dp = [0]*100001
+    dp[1] = 1
+    dp[2] = 3
+    dp[3] = 10
+    dp[4] = 23
+    dp[5] = 62
+    dp[6] = 170
+    
+    for i in range(7, n+1):
+        dp[i] = (dp[i-1] + dp[i-2] * 2 + dp[i-3] * 6 + dp[i-4] - dp[i-6]) % 1000000007 
+    
+    return dp[n]
