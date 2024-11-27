45183461d8e07620fecb3e21baf4e09278e7f9db
bu119 <109335452+bu119@users.noreply.github.com>
2024-11-27 23:29:36 +0900

    [level 2] Title: 호텔 대실, Time: 2.03 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,25 +1,24 @@
-import heapq
-
 def solution(book_time):
-    # 최소한의 객실만을 사용하여 예약 손님들을 받
-    # 한 번 사용한 객실은 퇴실 시간을 기준으로 10분간 청소
     n = len(book_time)
     # 분으로 변환하여 저장
-    book_times = []
-    # 시간 -> 분으로 변환
-    for i in range(n):
-        time = []
-        for j in range(2):       
-            hour, minute = map(int, book_time[i][j].split(":"))
-            time.append((hour*60 + minute))
-        book_times.append(time)
-    # 정렬
-    book_times.sort()
-    # 방마다 끝나는 시간 저장 (heap)
-    rooms = []
-    for start, end in book_times:
-        if rooms and rooms[0] + 10 <= start:
-            heapq.heappop(rooms)
-        heapq.heappush(rooms, end)
+    times = []
+    # 시간 -> 분으로 변환하여 저장
+    for start, end in book_time:
+        # 시작 시각 저장 (1)
+        start_hour, start_minute = map(int, start.split(":"))
+        times.append((start_hour*60 + start_minute, 1))
+        # 종료 시각 + 10분 청소 저장 (-1) 
+        end_hour, end_minute = map(int, end.split(":"))
+        times.append((end_hour*60 + end_minute + 10, -1))
 
-    return len(rooms)
+    # 시간을 기준으로 정렬
+    times.sort()
+    # 최대 방 개수 저장
+    answer = 0
+    # 현재 방 개수 저장
+    room_cnt = 0
+    for time, room in times:
+        room_cnt += room
+        answer = max(answer, room_cnt)
+
+    return answer
