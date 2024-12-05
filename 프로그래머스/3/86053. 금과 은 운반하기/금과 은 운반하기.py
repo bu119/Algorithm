a2877cf3eeb1dec87c56300bf45875d7d22d2a79
bu119 <109335452+bu119@users.noreply.github.com>
2024-12-05 21:29:05 +0900

    [level 3] Title: 금과 은 운반하기, Time: 5271.94 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
@@ -1,56 +1,39 @@
 def solution(a, b, g, s, w, t):
-    start = 0
-    end = (10**9) * (10**5) * 4
-    answer = (10**9) * (10**5) * 4
-    
     # 도시 수
     n = len(g)
+    # 최소 시간
+    start = 0
+    # 최악의 시간: 운반가능 무게는 최소, 필요한 광물(금,은)과 트럭 당 이동시간은 최대 
+    end = 1 *(10**9 * 2) * (10**5 * 2)
+    # 트럭 당 운반 가능 무게(최소) * 필요한 광물 * (금, 은) * 트럭 당 편도 이동 시간(최대) * 왕복
+    answer = 1 *(10**9 * 2) * (10**5 * 2)
     
-    # 이진 탐색
+    # 이분 탐색
     while start <= end:
+        # 소요 시간 탐색
+        mid = (start + end) // 2
+        # mid 시간동안 전체 도시에서 운반한 금, 은, 금+은 총 무게 저장
         gold = 0
         silver = 0
         total = 0
-        
-        mid = (start + end) // 2
-
+        # mid 시간동안 각 도시에서 운반한 광물 계산
         for i in range(n):
-            ng = g[i]
-            ns = s[i]
-            nw = w[i]
-            nt = t[i]
-
-            # 이동 가능 수 (왕복)
-            cnt = mid // (nt * 2)
-            # 이동 가능 수 (편도)
-            if mid % (nt * 2) >= nt:
+            # 현재 도시에서 이동 가능한 횟수 저장 (왕복)
+            cnt = mid // (t[i]*2)
+            # 편도로 한번 더 갈 수 있으면 1 추가
+            if mid % (t[i]*2) >= t[i]:
                 cnt += 1
-
-            # 주어지 시간 내 최대 적재 가능량 누적하기
-            maxW = cnt * nw
-            
-            # gold 값 업데이트
-            if ng < maxW:
-                gold += ng
-            else:
-                gold += maxW
-
-            # silver 값 업데이트
-            if ns < maxW:
-                silver += ns
-            else:
-                silver += maxW
-
-            # total 값 업데이트
-            if ng + ns < maxW:
-                total += ng + ns
-            else:
-                total += maxW
-
-        if total >= a + b and gold >= a and silver >= b:
+                
+            # 현재 도시에서 이동 가능한 광물 무게
+            weight = cnt * w[i]
+            # 현재 도시에서 운반한 광물의 무게 누적
+            gold += min(g[i], weight)
+            silver += min(s[i], weight)
+            total += min(g[i] + s[i], weight)
+        # 필요한 무게 이동 가능하면 (운반한 금, 은, 금+은이 필요한 무게 이상인지 확인)
+        if gold >= a and silver >= b and total >= a + b:
             end = mid - 1
-            answer = min(answer, mid)
         else:
             start = mid + 1
-    
-    return answer
+
+    return start
