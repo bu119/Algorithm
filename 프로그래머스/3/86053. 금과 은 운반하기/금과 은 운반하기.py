a962a2d062bd80845c1752b1bca4459221010567
bu119 <109335452+bu119@users.noreply.github.com>
2024-12-11 23:26:35 +0900

    [level 3] Title: 금과 은 운반하기, Time: 4728.26 ms, Memory: 22.3 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.py
@@ -1,9 +1,9 @@
 def solution(a, b, g, s, w, t):
     
     def transport_mineral(time):
-        gold = 0
-        silver = 0
-        total = 0
+        max_gold = 0
+        max_silver = 0
+        max_total = 0
         # 해당 시간 안에 이동가능한 각 도시의 광물 합 
         for i in range(n):
             # 해당 도시 트럭이 시간 안에 몇번 이동 가능한 지
@@ -13,14 +13,14 @@ def solution(a, b, g, s, w, t):
                 move_cnt += 1
             # 이동 가능한 광물의 전체 무게
             move_weight = move_cnt * w[i]
-            # 담을 수 있는 전체 무게
-            total += min(g[i]+s[i], move_weight)
+            # 최대로 담을 수 있는 전체 무게
+            max_total += min(g[i]+s[i], move_weight)
             # 최대로 담을 수 있는 금 무게
-            gold += min(g[i], move_weight)
+            max_gold += min(g[i], move_weight)
             # 최대로 담을 수 있는 은 무게
-            silver += min(s[i], move_weight)
+            max_silver += min(s[i], move_weight)
         # 해당 시간에서 광물이 a, b 만큼 새 도시로 이동가능 한 지 체크
-        if a <= gold and b <= silver and a+b <= total:
+        if a <= max_gold and b <= max_silver and a+b <= max_total:
             # 이동 가능
             return True
         # 이동 불가능
@@ -34,15 +34,15 @@ def solution(a, b, g, s, w, t):
     start = 0
     end = 1 * (10**5) * 2 * (10**9) * 2
     # 이분 탐색 (최소 시간 ~ 최대 시간 탐색)
-    while start <= end:
+    while start < end:
         # 중간 시간
         mid = (start + end) // 2
         # 현재 시간에 이동 가능한 광물 계산해서 이동 가능한지 체크
         # 광물 이동 가능하면 최대 범위 줄이기
         if transport_mineral(mid):
-            end = mid -1
+            end = mid
         else:
             # 이동 불가능 하면 최소 범위 올리기
             start = mid + 1
             
-    return start
+    return end
