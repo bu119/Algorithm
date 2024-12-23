5a744aece772a9a5b2e3f5ed699d3ca12589bfd9
bu119 <109335452+bu119@users.noreply.github.com>
2024-12-23 19:12:08 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,32 +1,20 @@
-s = input()
+# 예시) <ab cd>ef gh<ij>
+s = input().split("<")
+# 예시) [ab cd>ef gh, ij>]
 ans = ""
-# "<"가 등장하면 True
-tag_flag = False
-# 단어 저장 -> 뒤집기
-string = ""
-for char in s:
-    # 태그 ("<"를 만나면 시작)
-    if tag_flag:
-        # 태그는 바로 저장 ("<" 도 그대로 저장)
-        ans += char
-    # 단어
-    else:
-        # 태그 시작 or 공백 -> 새로운 단어 시작
-        if char == "<" or char == " ":
-            # 단어 뒤집기
-            ans += string[::-1]
-            # 저장된 단어 초기화
-            string = ""
-            # "<" or " " 그대로 저장
-            ans += char
-        else:
-            # 단어 저장
-            string += char
-
-    if char == "<" or char == ">":
-        tag_flag = not tag_flag
-
-if string:
-    ans += string[::-1]
-
+for string in s:
+    # 예시) ab cd>ef gh
+    # 태그 존재
+    if ">" in string:
+        # 태그, 단어 분리
+        tag, string = string.split(">")
+        # 예시) ab cd, ef gh
+        # 태그 저장
+        ans += "<" + tag + ">"
+        # 예시) <ab cd>
+    # 단어 분리 -> 뒤집기
+    words = string.split()
+    # 예시) [ef, gh]
+    ans += " ".join(w[::-1] for w in words)
+    # 예시) fe hg
 print(ans)
