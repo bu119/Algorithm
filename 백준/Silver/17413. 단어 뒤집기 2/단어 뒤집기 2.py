099f3e87ddf910e91a512c2cb28d3da7a0645873
bu119 <109335452+bu119@users.noreply.github.com>
2024-12-23 18:44:33 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 188 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,30 +1,29 @@
 s = input()
-n = len(s)
 ans = ""
 # "<"가 등장하면 True
 tag_flag = False
 # 단어 저장 -> 뒤집기
 string = ""
-for i in range(n):
+for char in s:
     # 태그 ("<"를 만나면 시작)
     if tag_flag:
         # 태그는 바로 저장 ("<" 도 그대로 저장)
-        ans += s[i]
+        ans += char
     # 단어
     else:
         # 태그 시작 or 공백 -> 새로운 단어 시작
-        if s[i] == "<" or s[i] == " ":
+        if char == "<" or char == " ":
             # 단어 뒤집기
             ans += string[::-1]
             # 저장된 단어 초기화
             string = ""
             # "<" or " " 그대로 저장
-            ans += s[i]
+            ans += char
         else:
             # 단어 저장
-            string += s[i]
+            string += char
 
-    if s[i] == "<" or s[i] == ">":
+    if char == "<" or char == ">":
         tag_flag = not tag_flag
 
 if string:
