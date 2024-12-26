16771789c502297e402712d1098dfe23578186ce
bu119 <109335452+bu119@users.noreply.github.com>
2024-12-27 01:16:06 +0900

    [Gold V] Title: A와 B, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,16 +1,13 @@
 s = input()
 t = input()
-n = len(s)
-m = len(t)
 
-while n < m:
+while len(s) < len(t):
     # 문자열의 뒤에 A를 추가 -> A 제거
-    if t[m-1] == "A":
-        t = t[:m-1]
+    if t[-1] == "A":
+        t = t[:-1]
     # 문자열을 뒤집고 뒤에 B를 추가 -> B 삭제 -> 뒤집
     else:
-        t = t[:m-1][::-1]
-    m -= 1
+        t = t[:-1][::-1]
 
 if s == t:
     print(1)
