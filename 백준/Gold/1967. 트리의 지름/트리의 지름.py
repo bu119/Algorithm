f59734ede7ae7b9cabf579ad234f52b0f7c2abba
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-02 18:20:01 +0900

    [Gold IV] Title: 트리의 지름, Time: 56 ms, Memory: 35944 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,44 +1,45 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-# 특정 노드에서 각 노드로 길이 찾는 함수
-def dfs(node, ssum):
-    for next_node, nw in tree[node]:
-        if visited[next_node] == -1:
-            visited[next_node] = ssum + nw
-            dfs(next_node, ssum+nw)
+# 트리 길이 찾는 함수
+def dfs(node):
+    # 리프 노드 (최하단 노드)
+    if not tree[node]:
+        # 마지막 노드라 더 해질 길이가 없음
+        return 0
+    # 자식이 하나뿐인 노드 (길이 하나뿐)
+    elif len(tree[node]) == 1:
+        child, weight = tree[node][0]
+        # 길의 길이 가져오기
+        dist[node] = dfs(child) + weight
+        # 반환 되어 위로 더해질 때는 길이 하나라 그대로 더해짐
+        return dist[node]
+    # 자식이 여러개 일 때 최대 길이 2개 찾아 연결하기
+    else:
+        # node 노드에서 갈라져 나올 수 있는 길이 저장
+        child_lengths = []
+        # node 노드 기준으로 갈라지는 길 탐색
+        for child, weight in tree[node]:
+            # 현재 길에서의 길이 구하기
+            child_lengths.append(dfs(child) + weight)
+        # 최대 길이 2개 찾기
+        # 갈라진 길의 길이를 내림 차순 정렬
+        child_lengths.sort(reverse=True)
+        # 최대 길이 2개를 가져와 연결하기
+        dist[node] = child_lengths[0] + child_lengths[1]
+        # 반환되어 위로 더해질 때는 가장 긴 길이 가져가기
+        return child_lengths[0]
 
 
+# 트리의 지름: 트리에 존재하는 모든 경로들 중에서 가장 긴 것
 n = int(input())
-tree = [[] for _ in range(n+1)]
-# 자식이 0인 노드 -> 부모가 아닌 노드 (출발점)
-leaf_node = set(range(1, n+1))
-for _ in range(n-1):
+tree = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
     p, c, w = map(int, input().split())
-    # 양방향 이동
     tree[p].append((c, w))
-    tree[c].append((p, w))
-    # 자식있는 노드 삭제
-    leaf_node.discard(p)
-    
-# 특정 노드에서 각 노드의 길이 저장
-visited = [-1] * (n + 1)
-# 루트노드에서 출발
-visited[1] = 0
-# 루트노드에서 각 노드로 길이 찾기
-dfs(1, 0)
-# 최대 길이
-max_length = 0
-# 최대 길이 노드
-start = -1
-# 최대 길이 찾기 (리프노드만 탐색하면 됌)
-for i in leaf_node:
-    if max_length < visited[i]:
-        max_length = visited[i]
-        start = i
-# 루트노드에서 가장 먼 노드를 출발점으로 각 노드의 길이 저장
-visited = [-1] * (n + 1)
-visited[start] = 0
-dfs(start, 0)
-print(max(visited))
+# 각 노드에서의 최대 길이 저장
+dist = [0]*(n+1)
+# 루드노드부터 하위노드 탐색 (부모노드를 기준으로 탐색하여 길이 연결)
+dfs(1)
+print(max(dist))
