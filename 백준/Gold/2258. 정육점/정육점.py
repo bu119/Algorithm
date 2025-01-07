28c44d5a52a1b1efcf9d549c66423b3c9a8513e3
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-07 21:09:14 +0900

    [Gold IV] Title: 정육점, Time: 172 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/2258. 정육점/정육점.py
+++ 백준/Gold/2258. 정육점/정육점.py
@@ -2,44 +2,46 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-meat = []
+meat = dict()
 # 전체 무게 저장
 total_weight = 0
 for _ in range(n):
     # 각 고기 덩어리의 무게와 가격
     w, p = map(int, input().split())
-    # 가격, 무게 순으로 저장
-    meat.append((p, w))
+    # 가격 기준으로 저장
+    if p in meat:
+        meat[p].append(w)
+    else:
+        meat[p] = [w]
     total_weight += w
 # 최대 비용
 max_val = 2147483648
 ans = max_val
 # 필요한 양이상 구할 수 있으면
 if total_weight >= m:
+    # 무게 누적
+    sum_weight = 0
     # 가격 기준 오름차순 정렬 -> 싼 고기들은 얼마든지 덤으로 획득 가능
-    # 같은 가격이 있다면, 무게 기준 내림차순 정렬 -> 같은 가격은 비용 추가
-    meat = sorted(meat, key=lambda x: (x[0], -x[1]))
-    # 초기 값
-    cost = meat[0][0]
-    sum_weight = meat[0][1]
-    for i in range(1, n):
-        # 가격, 무게
-        price, weight = meat[i]
-        # 무게 누적
-        sum_weight += weight
-        # 가격이 같으면 누적
-        if meat[i-1][0] == price:
-            cost += price
-        else:
-            cost = price
-        # 필요한 고기 이상이면 탐색 종료
-        if sum_weight >= m:
-            # 같은 가격 두개 합한 것보다 그 다음 가격이 작을 수 있음
-            ans = min(ans, cost)
-            # 한 개 가격이면 더 이상 비교할 필요 없음
-            if cost == price:
+    for price in sorted(meat):
+        # 같은 가격이 있다면, 무게 기준 내림차순 정렬 -> 같은 가격은 비용 추가
+        meat[price].sort(reverse=True)
+        # 같은 가격 개수 저장
+        cnt = 0
+        for weight in meat[price]:
+            # 개수 추가
+            cnt += 1
+            # 무게 추가
+            sum_weight += weight
+            # 필요한 양 이상이면 탐색 종료
+            if sum_weight >= m:
+                # 같은 가격 두개 합한 것보다 그 다음 가격이 작을 수 있음
+                ans = min(ans, price * cnt)
+                # 같은 가격은 더 탐색할 필요 x -> 현재 비용이 최소
                 break
-
+        # 한 개 가격이면 더 이상 비교할 필요 없음
+        if cnt == 1 and ans != max_val:
+            break
+# 불가능하면
 if ans == max_val:
     ans = -1
 print(ans)
