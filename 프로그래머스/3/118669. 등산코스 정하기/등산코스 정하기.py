2c72574606b3f6d39b7709d204d0f248886d8ab3
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-12 16:29:54 +0900

    [level 3] Title: 등산코스 정하기, Time: 572.21 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,8 +1,8 @@
 import heapq
 
 def solution(n, paths, gates, summits):
-
-    def dijkstra():
+    
+    def dijkstra():       
         visited = [10000001]*(n+1)
         heap = []
         # 각 출발점에서 산봉우리 가는 길 탐색
@@ -11,22 +11,29 @@ def solution(n, paths, gates, summits):
             visited[gate] = 0
         
         while heap:
-            dist, now = heapq.heappop(heap)
-            # 저장된 거리가 더 작거나 산봉우리를 만나면 탐색 안함
-            if visited[now] < dist or now in set_summits:
+            curr_time, now = heapq.heappop(heap)
+            # 저장된 시간이 더 작거나 산봉우리를 만나면 탐색 안함
+            if visited[now] < curr_time or now in set_summits:
                 continue
-            
-            for next_dist, next in graph[now]:
+
+            for next_time, next in graph[now]:
                 # 출발점을 만나면 탐색 안함
                 if next in set_gates:
                     continue
-                max_dist = max(dist, next_dist)
+                max_time = max(curr_time, next_time)
                 # intensity(가장 긴 시간) 작으면 값 갱신
-                if max_dist < visited[next]:
-                    visited[next] = max_dist
-                    heapq.heappush(heap, (max_dist, next))
+                if max_time < visited[next]:
+                    visited[next] = max_time
+                    heapq.heappush(heap, (max_time, next))
+                    
+        # 최소 intensity 찾기
+        min_intensity = [0, 10000001]
+        for summit in sorted(summits):
+            if visited[summit] < min_intensity[1]:
+                min_intensity[1] = visited[summit]
+                min_intensity[0] = summit
         
-        return visited
+        return min_intensity
 
     
     # 등산로 양방향 그래프로 저장
@@ -37,15 +44,8 @@ def solution(n, paths, gates, summits):
     
     set_summits = set(summits)
     set_gates = set(gates)
-    
-    answer = [0, 10000001]
-    summits.sort()
-    # 각 산봉우리까지 intensity 찾기
-    intensity = dijkstra()
+
     # 최소 intensity 찾기
-    for summit in summits:
-        if intensity[summit] < answer[1]:
-            answer[0] = summit
-            answer[1] = intensity[summit]
+    answer = dijkstra()
 
     return answer
