b5cbe9bdf8a3857252b5ee9293431d91ecdba727
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-13 21:49:31 +0900

    [Gold IV] Title: 테트로미노, Time: 1072 ms, Memory: 117456 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,46 +1,36 @@
-import sys
-input = sys.stdin.readline
-
-def dfs(i, j, ssum, idx):
+# 테트로미노는 회전이나 대칭을 시켜도 된다 -> 4개의 정사각형을 4방향 탐색으로 4번 움직인 모양
+def dfs(idx, x, y, ssum):
     global ans
 
     if idx == 4:
-        if ans < ssum:
-            ans = ssum
+        ans = max(ans, ssum)
         return
 
     for k in range(4):
-        ni = i + di[k]
-        nj = j + dj[k]
-
-        if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj]:
-            visited[ni][nj] = 1
-            dfs(ni, nj, ssum + arr[ni][nj], idx + 1)
-            visited[ni][nj] = 0
-
-            # ㅜ 모양, 깊이 2에서 한번 더 탐색
+        nx = x + di[k]
+        ny = y + dj[k]
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            # 방문 체크
+            visited[nx][ny] = 1
+            dfs(idx+1, nx, ny, ssum+graph[nx][ny])
+            # ㅗ,ㅜ,ㅏ,ㅓ 모양은 깊이 2 위치에서 두 블록 탐색 -> (x, y) 위치에서 두번 탐색
             if idx == 2:
-                visited[ni][nj] = 1
-                dfs(i, j, ssum + arr[ni][nj], idx + 1)
-                visited[ni][nj] = 0
+                dfs(idx + 1, x, y, ssum + graph[nx][ny])
+            visited[nx][ny] = 0
 
 
 n, m = map(int, input().split())
-
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-# N×M인 종이 위에 테트로미노 하나를 놓으려고 한다.
-# dfs 깊이 4 탐색
+graph = [list(map(int, input().split())) for _ in range(n)]
+# 동 남 서 북
 di = [0, 1, 0, -1]
 dj = [1, 0, -1, 0]
 
-visited = [[0]*m for _ in range(n)]
 ans = 0
-
+# 방문 체크
+visited = [[0]*m for _ in range(n)]
 for i in range(n):
     for j in range(m):
         visited[i][j] = 1
-        dfs(i, j, arr[i][j], 1)
+        dfs(1, i, j, graph[i][j])
         visited[i][j] = 0
-
 print(ans)
