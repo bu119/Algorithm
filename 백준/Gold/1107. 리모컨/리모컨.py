5820ca94220ae81926e341e52641ade848eeef16
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-15 21:47:10 +0900

    [Gold V] Title: 리모컨, Time: 1020 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,18 @@
+n = int(input())
+broken_button_cnt = int(input())
+if broken_button_cnt:
+    broken_button = set(input().split())
+else:
+    broken_button = set()
+# 지금 보고 있는 채널은 100번
+# 최소 버튼 누르는 횟수
+ans = abs(100-n)
+if n != 100:
+    for channel in range(1000001):
+        str_channel = str(channel)
+        buttons = set(list(str_channel))
+        if not (broken_button & buttons):
+            cnt = len(str_channel) + abs(n-channel)
+            if cnt < ans:
+                ans = cnt
+print(ans)
