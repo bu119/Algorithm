e906fd1ea35bc36c79f661868781c2ba12a2d053
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-15 22:57:18 +0900

    [Gold V] Title: 리모컨, Time: 1020 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -10,11 +10,9 @@ ans = abs(100-n)
 if n != 100:
     for channel in range(1000001):
         str_channel = str(channel)
-        is_possible = True
-        for button in str_channel:
-            if button in broken_button:
-                is_possible = False
-                break
-        if is_possible:
-            ans = min(ans, len(str_channel) + abs(n-channel))
+        buttons = set(list(str_channel))
+        if not (broken_button & buttons):
+            cnt = len(str_channel) + abs(n-channel)
+            if cnt < ans:
+                ans = cnt
 print(ans)
