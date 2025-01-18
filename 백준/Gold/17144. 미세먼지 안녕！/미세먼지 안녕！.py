2e174dce91ff7132dce81a82bdedb81d7ce9feb7
bu119 <109335452+bu119@users.noreply.github.com>
2025-01-18 12:44:11 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 316 ms, Memory: 112564 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,85 +1,81 @@
 import sys
 input = sys.stdin.readline
 
-# 먼지 퍼트리기
-def spreadDust():
-    global dust
+# 미세먼지 확산
+def spread_dust():
+    # 확산될 먼지 저장
+    new_house = [[0]*c for _ in range(r)]
+    # 먼지 위치 찾기
+    for x in range(r):
+        for y in range(c):
+            # 먼지가 있으면 탐색
+            if house[x][y] == 0:
+                continue
+            # 먼지 저장
+            new_house[x][y] += house[x][y]
+            # 청정기가 있으면 탐색 안함
+            if house[x][y] == -1:
+                continue
+            # 확산될 먼지
+            dust = house[x][y] // 5
+            # 확산 될 양이 없으면 확산 안함
+            if dust <= 0:
+                continue
+            # 확산
+            for k in range(4):
+                nx = x + di[k]
+                ny = y + dj[k]
+                if 0 <= nx < r and 0 <= ny < c and house[nx][ny] != -1:
+                    new_house[nx][ny] += dust
+                    new_house[x][y] -= dust
+    return new_house
 
-    while dust:
-        x, y, v = dust.pop()
+# 공기청정기 작동
+def operate_air_purifier():
+    # 공기청정기 작동 방향
+    for d in ["UP", "DOWN"]:
+        # 방향에 따른 경계
+        sr, sc, er, ec = air_purifier_boundary[d]
+        for x, y, k in air_purifier_dir[d]:
+            # 한 방향씩 탐색
+            while sr <= x + di[k] <= er and sc <= y + dj[k] <= ec:
+                # 다음 위치가 공기청정기이면 0
+                if house[x + di[k]][y + dj[k]] == -1:
+                    house[x][y] = 0
+                    break
+                # 다음 위치 먼지 가져와 현재 위치에 덮어쓰기
+                house[x][y] = house[x + di[k]][y + dj[k]]
+                x += di[k]
+                y += dj[k]
 
-        for k in range(4):
-            nx = x + di[k]
-            ny = y + dj[k]
-            if 0 <= nx < r and 0 <= ny < c and room[nx][ny] > -1:
-                room[nx][ny] += (v // 5)
-                room[x][y] -= (v // 5)
-
-#공기청정기돌리기
-def operateCleaner(dir):
-    global cleaner
-    # 기계 위치
-    if dir == 'up':
-        dix = dx
-        djy = dy
-        x, y = cleaner[0]
-        sx, ex = 0, x
-        x -= 1
-    else:
-        dix = di
-        djy = dj
-        x, y = cleaner[1]
-        sx, ex = x, r-1
-        x += 1
-
-    # 방향
-    k = 0
-    while k < 4:
-        nx = x + dix[k]
-        ny = y + djy[k]
-        if sx <= nx <= ex and 0 <= ny < c:
-            if room[nx][ny] == -1:
-                room[x][y] = 0
-                return
-            room[x][y] = room[nx][ny]
-            x, y = nx, ny
-        else:
-            k += 1
+# 남아있는 먼지 출력
+def count_dust():
+    cnt = 0
+    for x in range(r):
+        for y in range(c):
+            if house[x][y] > 0:
+                cnt += house[x][y]
+    return cnt
 
 
 r, c, t = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(r)]
-
-# 위 순환 - 시계로 먼지 이동 저장
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-# 아래순환 - 반시계로 먼지 이동 저장
-di = [1,0,-1,0]
-dj = [0,1,0,-1]
-
-# 기계 위치
-cleaner = []
-for tc in range(t):
-    # 먼지가 있는 위치 찾기
-    dust = []
-    for i in range(r):
-        for j in range(c):
-            if room[i][j] > 0:
-                dust.append((i, j, room[i][j]))
-            if room[i][j] == -1 and tc == 0:
-                cleaner.append((i, j))
-    # 먼지 퍼트리기
-    spreadDust()
-    # print(room)
-    # 기계 작동
-    operateCleaner('up')
-    operateCleaner('down')
-    # print(room)
-    # print()
-
-ans = 2
-for z in room:
-    ans += sum(z)
-
-print(ans)
+house = [list(map(int, input().split())) for _ in range(r)]
+air_purifier = []
+for i in range(r):
+    if house[i][0] == -1:
+        air_purifier.append(i)
+        if len(air_purifier) == 2:
+            break
+# 동 남 서 북
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+apr1 = air_purifier[0]
+apr2 = air_purifier[1]
+# 청정기로 들어오는 위치부터 가동 반대 방향으로 탐색하면서 다음 위치 먼지 가져와 덮어쓰기
+# 탐색을 시작할 위치, 탐색 방향 (청정기 이후 위치부터 잡기)
+air_purifier_dir = {"UP": [(apr1-1, 0, 3), (0, 0, 0), (0, c-1, 1), (apr1, c-1, 2)], "DOWN": [(apr2+1, 0, 1), (r-1, 0, 0), (r-1, c-1, 3), (apr2, c-1, 2)]}
+air_purifier_boundary = {"UP": (0, 0, apr1, c-1), "DOWN": (apr2, 0, r-1, c-1)}
+for _ in range(t):
+    house = spread_dust()
+    operate_air_purifier()
+print(count_dust())
