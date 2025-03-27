9ac66920c02e826b44028626759c07ffc538246b
bu119 <109335452+bu119@users.noreply.github.com>
2025-03-27 18:50:35 +0900

    [level 3] Title: 불량 사용자, Time: 62.74 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -12,18 +12,18 @@ def solution(user_id, banned_id):
         return True
     
     # 불량 사용자 후보 가져오기
-    def find_banned_candidates():
+    def find_banned_candidates(banned_users, users):
         # 불량 사용자 후보 저장
         candidates = dict()
-        for i in range(n):
-            x = len(banned_id[i])
-            candidates[i] = set()
-            for j in range(m):
-                y = len(user_id[j])
+        for banned_user in banned_users:
+            x = len(banned_user)
+            candidates[banned_user] = set()
+            for candidate in users:
+                y = len(candidate)
                 # 아이디 길이가 같고 불량 사용자 후보이면
-                if x == y and is_banned_user(banned_id[i], user_id[j]):
+                if x == y and is_banned_user(banned_user, candidate):
                     # 불량 후보로 저장
-                    candidates[i].add(j)
+                    candidates[banned_user].add(candidate)
         return candidates
     
     # 제재 아이디 목록 찾기
@@ -32,19 +32,17 @@ def solution(user_id, banned_id):
             answer.add(tuple(sorted(visited)))
             return
         # 불량 사용자 후보 탐색
-        for candidate in banned_candidates[banned_idx]:
-            if candidate not in visited:
-                visited.add(candidate)
+        for user in banned_candidates[banned_id[banned_idx]]:
+            if user not in visited:
+                visited.add(user)
                 dfs(banned_idx+1)
-                visited.remove(candidate)
+                visited.remove(user)
 
     # 제재 아이디 개수
     n = len(banned_id)
-    # 사용자 수
-    m = len(user_id)
     # 제재 아이디 목록 저장
     answer = set()
-    banned_candidates = find_banned_candidates()
+    banned_candidates = find_banned_candidates(banned_id, user_id)
     # 현재 탐색에서 선정된 제재 아이디 방문 체크
     visited = set()
     dfs(0)
