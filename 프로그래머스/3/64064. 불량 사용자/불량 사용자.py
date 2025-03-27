7e6fea35054d4d280bc4755dbdb5bf74e96c9aa3
bu119 <109335452+bu119@users.noreply.github.com>
2025-03-27 17:57:15 +0900

    [level 3] Title: 불량 사용자, Time: 311.78 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,50 +1,47 @@
 def solution(user_id, banned_id):
-    # 불량 사용자에 매핑되는 지 확인
-    def is_mappable(x, y):
-        # x: 사용자, y: 불량 사용자
-        length = len(x)
-        for k in range(length):
-            if y[k] != "*" and x[k] != y[k]:
+    n = len(banned_id)
+    banned_user = set()
+    users = set(user_id)
+    # 불량 사용자 목록에 해당하는 사용자 목록
+    visited = set()
+    
+    
+    # 불량 사용자 a에 사용자 b가 해당되는 지 확인 (c는 길이)
+    def is_same(a, b, c):
+        for x in range(c):
+            # "*" 는 비교 x
+            if a[x] == "*":
+                continue
+            # 문자가 다르면 False 반환
+            if a[x] != b[x]:
                 return False
         return True
-
-    # 제재 아이디 목록
-    def dfs(idx):
-        nonlocal answer
-        # 불량 사용자를 모두 탐색하면 매핑된 사용자들 반환
-        if idx == m:
-            # 중복 방지
-            answer.add(tuple(sorted(visited)))
+    
+    def dfs(banned_idx):
+        nonlocal visited
+        if banned_idx == n:
+            visited.add(tuple(sorted(banned_user)))
             return
-        # 불량 사용자에 매핑되는 사용자 탐색
-        for k in bad_candidates[banned_id[idx]]:
-            if k not in visited:
-                visited.add(k)
-                dfs(idx+1)
-                visited.discard(k)
+        # 불량 사용자 길이 *
+        m = len(banned_id[banned_idx])
+        for user in users:
+            # 비교 사용자 길이
+            k = len(user)
+            # 길이 비교
+            if m != k:
+                continue
+            # 길이가 같으면 불량 사용자인지 탐색
+            if is_same(banned_id[banned_idx], user, m):
+                # 같으면 불량사용자에 포함
+                banned_user.add(user)
+                # 응모 사용자에서 제거
+                users.remove(user)
+                # 다음 불량 사용자 아이디 목록 탐색
+                dfs(banned_idx+1)
+                # 재귀 나오면 복구
+                banned_user.remove(user)
+                users.add(user)
                 
-        
-    # 불량사용자: {후보}
-    bad_candidates = dict()
-    n = len(user_id)
-    m = len(banned_id)
-    # 불량 사용자 기준으로 매핑되는 사용자 탐색
-    for i in range(m):
-        # 불량 사용자
-        bad_user = banned_id[i]
-        bad_candidates[bad_user] = set()
-        for j in range(n):
-            # 사용자
-            user = user_id[j]
-            # 불량 사용자에 매핑되는지 체크
-            if len(bad_user) == len(user) and is_mappable(user, bad_user):
-                # 매핑되면 사용자 인덱스 저장
-                bad_candidates[bad_user].add(j)
-    # 제재 목록 저장
-    answer = set()
-    # 방문 체크
-    visited = set()
-    # 제재 목록 탐색
+                
     dfs(0)
-        
-    return len(answer)
+    return len(visited)
