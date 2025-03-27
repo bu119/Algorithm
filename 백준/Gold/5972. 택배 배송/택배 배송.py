e92cd4f4dbcc5375782ffc5a2e44ba873cbb0b50
bu119 <109335452+bu119@users.noreply.github.com>
2025-03-27 17:08:00 +0900

    [Gold V] Title: 택배 배송, Time: 208 ms, Memory: 52284 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,28 +1,31 @@
 import sys, heapq
 input = sys.stdin.readline
 
-def dijkstra(x):
-    dist = [50000001] * (n+1)
-    heap = [(0, x)]
-    dist[x] = 0
-    while heap:
-        currCow, x = heapq.heappop(heap)
+def dijkstra():
+    visited = [50000001]*(n+1)
+    heap = [(0, 1)]
+    visited[1] = 0
 
-        if dist[x] < currCow:
+    while heap:
+        cows, curr = heapq.heappop(heap)
+
+        if visited[curr] < cows:
             continue
 
-        for next, nextCow in graph[x]:
-            totalCow = currCow + nextCow
-            if totalCow < dist[next]:
-                dist[next] = totalCow
-                heapq.heappush(heap, (totalCow, next))
-    return dist[n]
+        for next, cow in graph[curr]:
+            ssum_cows = visited[curr] + cow
+            if ssum_cows < visited[next]:
+                visited[next] = ssum_cows
+                heapq.heappush(heap, (ssum_cows, next))
+    return visited[n]
 
-
+# 최소 한의 소 만나기(최단경로)
+# 1 -> N 으로 이동
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
+    # a-b사이에 c마리의 소 존재
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
     graph[b].append((a, c))
-print(dijkstra(1))
+print(dijkstra())
