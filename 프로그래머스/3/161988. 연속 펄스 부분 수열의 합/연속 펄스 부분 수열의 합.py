cae38c7451fc027c2454fff65f4a1d0aba2e5a33
bu119 <109335452+bu119@users.noreply.github.com>
2025-04-01 13:28:55 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 386.58 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,7 +1,4 @@
 def solution(sequence):
-    # 연속 펄스 부분 수열의 합 중 가장 큰 것
-    # 연속 펄스 부분 수열: 연속 부분 수열 * 펄스 수열
-    # 펄스 수열: 1 또는 -1로 시작하면서 1과 -1이 번갈아 나오는 수열
     n = len(sequence)
     answer = 0
     # [1,-1, ...]
@@ -9,6 +6,7 @@ def solution(sequence):
     # [-1, 1, ...]
     odd = 0
     for i in range(n):
+        # 펄스 수열 곱한 수
         pulse_num = (sequence[i] if i%2 == 0 else -sequence[i])
         even = max(pulse_num, even + pulse_num)
         odd = min(pulse_num, odd + pulse_num)
