6f7215c47131a84045422862a9781d2fdd56aa22
bu119 <109335452+bu119@users.noreply.github.com>
2025-04-01 13:21:26 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 313.00 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -3,10 +3,14 @@ def solution(sequence):
     # 연속 펄스 부분 수열: 연속 부분 수열 * 펄스 수열
     # 펄스 수열: 1 또는 -1로 시작하면서 1과 -1이 번갈아 나오는 수열
     n = len(sequence)
-    dp_even = [0]*n
-    dp_odd = [0]*n
+    answer = 0
+    # [1,-1, ...]
+    even = 0
+    # [-1, 1, ...]
+    odd = 0
     for i in range(n):
-        num = (1 if i%2 == 0 else -1)
-        dp_even[i] = max(sequence[i]*num, dp_even[i-1] + sequence[i]*num)
-        dp_odd[i] = max(-sequence[i]*num, dp_odd[i-1] - sequence[i]*num)
-    return max(max(dp_even), max(dp_odd))
+        pulse_num = (sequence[i] if i%2 == 0 else -sequence[i])
+        even = max(pulse_num, even + pulse_num)
+        odd = min(pulse_num, odd + pulse_num)
+        answer = max(answer, even, -odd)
+    return answer
