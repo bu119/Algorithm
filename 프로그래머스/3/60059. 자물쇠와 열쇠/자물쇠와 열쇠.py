9e8461707e049601bbb7e0da0cb6b6d7d8c13bfd
bu119 <109335452+bu119@users.noreply.github.com>
2025-04-09 23:05:22 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 334.91 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,46 +1,49 @@
-from copy import deepcopy
-
 def solution(key, lock):
-    # 자물쇠 영역 내에서 열쇠와 자물쇠가 일치하는 지 확인
+    # 열쇠 회전
+    def rotate_90(matrix):
+        # 90도 회전
+        return [list(row) for row in zip(*matrix[::-1])]
+    
+    # 열쇠와 자물쇠가 일치하는 지 확인
     def check_match(area):
         for i in range(n):
             for j in range(n):
-                if area[i + m-1][j + m-1] != 1:
+                if area[i + m - 1][j + m - 1] != 1:
                     return False
         return True
     
-    # 이동한 열쇠를 회전시켜 자물쇠 열 수 있는 지 체크
+    # 열쇠를 회전 및 이동시켜 자물쇠 열 수 있는 지 확인
     def can_unlock():
-        # 행과 열 모두 0 ~ m+n-2 까지 이동
-        for dx in range(m + n - 1):
-            for dy in range(m + n - 1):
-                # 열쇠 회전
-                for k in range(4):
-                    # 비교할 새 자물쇠
-                    lock_area = deepcopy(padded_lock)
+        rotated_key = key
+        for _ in range(4):
+            # 행과 열 모두 0 ~ m+n-2 까지 이동
+            for dx in range(m + n - 1):
+                for dy in range(m + n - 1):
+                    # 자물쇠 복사
+                    lock_area = [row[:] for row in padded_lock]
+                    # 열쇠를 자물쇠에 덮기
                     for x in range(m):
                         for y in range(m):
-                            # 열쇠가 이동한 위치
-                            nx = x + dx
-                            ny = y + dy
-                            # 자물쇠에 맞춰보기
-                            lock_area[nx][ny] += rotated_keys[k][x][y]
+                            lock_area[dx + x][dy + y] += rotated_key[x][y]
                     # 열쇠와 자물쇠가 일치하는 지 확인
                     if check_match(lock_area):
                         return True
+            # 다음 회전
+            rotated_key = rotate_90(rotated_key)
         return False
 
 
     m = len(key)
     n = len(lock)
-    # 열쇠를 이동시키기 위해 자물쇠 영역 확장(n+2*(m-1))
-    padded_lock = [[0] * (n + 2 * (m - 1)) for _ in range(n + 2 * (m - 1))]
+    
+    # 자물쇠 확장 (n + 2*(m-1))
+    size = n + 2 * (m - 1)
+    padded_lock = [[0] * size for _ in range(size)]
+
+    # 자물쇠 중앙 배치
     for i in range(n):
         for j in range(n):
             padded_lock[i + m - 1][j + m - 1] = lock[i][j]
-    # 회전 각도별 열쇠 저장(90도씩 시계)
-    rotated_keys = {0: key}
-    for cnt in range(1, 4):
-        rotated_keys[cnt] = list(zip(*rotated_keys[cnt-1][::-1]))
-    # 자물쇠 열 수 있는 지
+            
+    # 열쇠로 자물쇠를 열수 있는 지 확인
     return can_unlock()
