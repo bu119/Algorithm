df7f0d74aa883e89a38140ee8103b9dfd73b7288
bu119 <109335452+bu119@users.noreply.github.com>
2025-04-19 21:29:10 +0900

    [level 2] Title: 지게차와 크레인, Time: 169.64 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -1,6 +1,6 @@
 def solution(storage, requests):
     def bfs(alpha, x, y):
-        nonlocal visited, dispatch_containers
+        nonlocal visited, answer
         
         stack = [(x, y)]
         visited[x][y] = 1
@@ -16,25 +16,33 @@ def solution(storage, requests):
                     if storage[nx][ny] == "":
                         stack.append((nx, ny))
                     elif storage[nx][ny] == alpha:
-                        dispatch_containers.add((nx, ny))
+                        # 꺼낼 때마다 answer 감소
+                        storage[nx][ny] = ""
+                        answer -= 1  
                         
     # 지게차: 출고 요청이 들어온 순간 접근 가능한 컨테이너를 꺼냄
     # 접근이 가능한 컨테이너란 4면 중 적어도 1면이 창고 외부와 연결된 컨테이너
     def use_forklift(alpha):
-        nonlocal dispatch_containers
+        nonlocal answer
 
         # 위아래 탐색
         for i in [0, n-1]:
             for j in range(m):
                 if storage[i][j] == alpha:
-                    dispatch_containers.add((i, j))
+                    # 꺼낼 때마다 answer 감소
+                    visited[i][j] = 1
+                    storage[i][j] = ""
+                    answer -= 1
                 elif storage[i][j] == "" and not visited[i][j]:
                     bfs(alpha, i, j)
         # 좌우 탐색
         for i in range(1, n-1):
             for j in [0, m-1]:
                 if storage[i][j] == alpha:
-                    dispatch_containers.add((i, j))
+                    # 꺼낼 때마다 answer 감소
+                    visited[i][j] = 1
+                    storage[i][j] = ""
+                    answer -= 1
                 elif storage[i][j] == "" and not visited[i][j]:
                     bfs(alpha, i, j)
                     
@@ -54,25 +62,21 @@ def solution(storage, requests):
     # 각 알파벳 위치 탐색
     for row in range(n):
         for col in range(m):
-                containers.setdefault(storage[row][col], set()).add((row, col))
+            containers.setdefault(storage[row][col], set()).add((row, col))
     
     for request in requests:
         # 알파벳 하나 -> 지게차
         # 같은 알파벳이 두 번 반복 -> 크레인
         if len(request) == 1:
-            # 꺼내는 컨테이너 위치 저장
-            dispatch_containers = set()
             # 방문 체크
             visited = [[0]*m for _ in range(n)]
             use_forklift(request)            
         else:
-            if request[0] in containers:
-                dispatch_containers = containers.pop(request[0])
-            else:
-                dispatch_containers = set()
-        # 꺼낸 컨테이너 제거
-        for row, col in dispatch_containers:
-            if storage[row][col] != "":
-                storage[row][col] = ""
-                answer -= 1  # 꺼낼 때마다 answer 감소
+            if request[0] not in containers:
+                continue
+            # 꺼낸 컨테이너 제거
+            for row, col in containers.pop(request[0]):
+                if storage[row][col] != "":
+                    storage[row][col] = ""
+                    answer -= 1  # 꺼낼 때마다 answer 감소
     return answer
