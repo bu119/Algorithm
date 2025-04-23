f24774fe42c0e9aaf1e0a1aebea7c3892a0e41bb
bu119 <109335452+bu119@users.noreply.github.com>
2025-04-23 19:29:12 +0900

    [Gold V] Title: 주사위 쌓기, Time: 328 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -10,27 +10,23 @@ for i in range(1, n+1):
     dices[i][d] = b
     dices[i][e] = c
     dices[i][f] = a
-
+# 최댓값 저장
 ans = 0
-# 첫 번째 주사위의 바닥 면
+# 1번 주사위의 바닥 면을 x로 했을 때
 for x in range(1, 7):
     ssum = 0
-    down = x
-    up = dices[1][down]
-    # 옆면 중 가장 큰 수 찾기
+    bottom = x
+    # 최종 기둥의 옆면 중 최댓값 찾기
     for y in range(1, n+1):
-        if 6 not in {up, down}:
+        top = dices[y][bottom]
+        if 6 not in {top, bottom}:
             ssum += 6
-        elif 5 not in {up, down}:
+        elif 5 not in {top, bottom}:
             ssum += 5
         else:
             ssum += 4
-
-        if y == n:
-            continue
-        down = up
-        up = dices[y+1][down]
-        
+        # 위로 쌓은 주사위의 바닥면
+        bottom = top
+    # 최댓값 갱신
     ans = max(ans, ssum)
-
 print(ans)
