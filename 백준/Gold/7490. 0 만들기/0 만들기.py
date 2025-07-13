5e266428428bf457593aec48d362fa00c645f8bd
bu119 <109335452+bu119@users.noreply.github.com>
2025-07-13 18:57:21 +0900

    [Gold V] Title: 0 만들기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -0,0 +1,49 @@
+def evaluates_to_zero(expression):
+    expression = expression.replace(" ", "")
+    result = 0
+    r_operand = ""
+    pre_operator = "+"
+    for e in expression:
+        # 피연산자: 숫자 누적
+        if e.isdigit():
+            r_operand += e
+        else:
+            # 연산자: 계산
+            if r_operand:
+                if pre_operator == "+":
+                    result += int(r_operand)
+                else:
+                    result -= int(r_operand)
+                r_operand = ""
+            pre_operator = e
+
+    if r_operand:
+        if pre_operator == "+":
+            result += int(r_operand)
+        else:
+            result -= int(r_operand)
+
+    if result == 0:
+        return True
+    return False
+
+
+def dfs(num, expression_str):
+    global items
+
+    if num == n:
+        # 문자열 수식 계산 -> 0 인지 확인
+        if evaluates_to_zero(expression_str):
+            print(expression_str)
+        return
+
+    for item in items:
+        dfs(num+1, expression_str + item + str(num+1))
+
+        
+t = int(input())
+items = sorted([" ", "+", "-"])
+for _ in range(t):
+    n = int(input())
+    dfs(1, "1")
+    print()
