1dfd80f364a4eee9a0c48a6ffbd1d6c263639296
bu119 <109335452+bu119@users.noreply.github.com>
2025-07-14 16:41:50 +0900

    [Gold V] Title: 0 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,50 +1,34 @@
-def evaluates_to_zero(expression):
-    result = 0
-    r_operand = ""
-    pre_operator = "+"
-    for e in expression:
-        if e == " ":
-            continue
-        # 피연산자: 숫자 누적
-        if e.isdigit():
-            r_operand += e
-        else:
-            # 연산자: 계산
-            if r_operand:
-                if pre_operator == "+":
-                    result += int(r_operand)
-                else:
-                    result -= int(r_operand)
-                r_operand = ""
-            pre_operator = e
-
-    if r_operand:
+# 문자열 수식 만들기 (모든 경우)
+def dfs(n, num, expression_str, result, pre_operator, r_operand):
+    # print(result)
+    if num == n:
         if pre_operator == "+":
             result += int(r_operand)
         else:
             result -= int(r_operand)
-
-    if result == 0:
-        return True
-    return False
-
-
-def dfs(num, expression_str):
-    global items
-
-    if num == n:
-        # 문자열 수식 계산 -> 0 인지 확인
-        if evaluates_to_zero(expression_str):
+        # 문자열 수식 결과 값이 0이면 수식 출력
+        if result == 0:
             print(expression_str)
         return
 
-    for item in items:
-        dfs(num+1, expression_str + item + str(num+1))
+    # " "일 때
+    dfs(n, num + 1, expression_str + " " + str(num + 1), result, pre_operator, r_operand + str(num+1))
 
-        
+    if pre_operator == "+":
+        # "+"일 때
+        dfs(n, num + 1, expression_str + "+" + str(num + 1), result + int(r_operand), "+", str(num+1))
+        # "-"일 때
+        dfs(n, num + 1, expression_str + "-" + str(num + 1), result + int(r_operand), "-", str(num+1))
+    else:
+        # "+"일 때
+        dfs(n, num + 1, expression_str + "+" + str(num + 1), result - int(r_operand), "+", str(num+1))
+        # "-"일 때
+        dfs(n, num + 1, expression_str + "-" + str(num + 1), result - int(r_operand), "-", str(num+1))
+
+
 t = int(input())
-items = sorted([" ", "+", "-"])
+# items = sorted([" ", "+", "-"])
 for _ in range(t):
     n = int(input())
-    dfs(1, "1")
+    dfs(n, 1, "1", 0, "+", "1")
     print()
