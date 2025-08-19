6ed4dd72995b2bd4da03142505831882d3c6807f
bu119 <109335452+bu119@users.noreply.github.com>
2025-08-19 18:35:10 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.52 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -2,38 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
+
         int[] number = {a, b, c, d};
         Arrays.sort(number); // 오름차순
         
-        // 4개 모두 같음: pppp
-        if (number[0] == number[3]) {
-            return 1111 * a;
-        }
-        // 3+1: pppq / qppp
-        if (number[0] == number[2]) {
-            return (10 * number[0] + number[3]) * (10 * number[0] + number[3]);
-        }
-        if (number[1] == number[3]) {
-        	return (10 * number[1] + number[0]) * (10 * number[1] + number[0]);
-        } 
-        
-        // 2+2: ppqq
-        if (number[0] == number[1] && number[2] == number[3]) {
-            return (number[0]+number[2]) * Math.abs(number[0]-number[2]);
-        }
-        
-        // 2+1+1: ppqr / qppr / qrpp
-        if (number[0] == number[1]) {
-            return number[2] * number[3];
-        }
-        if (number[1] == number[2]) {
-            return number[0] * number[3];
-        }        
-        if (number[2] == number[3]) {
-            return number[0] * number[1];
-        }        
-        
         // 4개 모두 다름: 최솟값
-        return number[0];
+        int answer = number[0]; 
+
+        if (number[0] == number[3]) {	// 4개 모두 같음: pppp
+            answer = 1111 * a;
+        } else if (number[0] == number[2]) {	// 3+1: pppq
+            answer = (10 * number[0] + number[3]) * (10 * number[0] + number[3]);
+        } else if (number[1] == number[3]) {	// 3+1: qppp
+        	answer = (10 * number[1] + number[0]) * (10 * number[1] + number[0]);
+        } else if (number[0] == number[1] && number[2] == number[3]) {	// 2+2: ppqq
+            answer = (number[0]+number[2]) * Math.abs(number[0]-number[2]);
+        } else if (number[0] == number[1]) {	// 2+1+1: ppqr
+            answer = number[2] * number[3];
+        } else if (number[1] == number[2]) {	// 2+1+1: qppr
+            answer = number[0] * number[3];
+        } else if (number[2] == number[3]) {	// 2+1+1:qrpp
+            answer = number[0] * number[1];
+        }        
+        // 4개 모두 다름: 최솟값
+        return answer;
     }
 }
