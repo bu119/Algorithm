4721543b74558b771cef9e0634391da40cf55dfb
bu119 <109335452+bu119@users.noreply.github.com>
2025-08-21 19:29:38 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 8.62 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,39 +1,28 @@
 def solution(mats, park):
-    # 해당 사이즈 돗자리 깔수 있는 지 확인하는 함수
-    def check_mat_space(x, y, size):
+    # 공원 전체에 해당 사이즈 돗자리 자리가 있는 지 확인
+    def can_place_mat(mat_size):
+        for i in range(n - mat_size + 1):
+            for j in range(m - mat_size + 1):
+                # 돗자리 들어가면 True 반환, 안들어가면 다른 자리 탐색
+                if park[i][j] == "-1" and can_fit_mat(i, j, mat_size):
+                    return True
+        return False
+
+    # (x,y)를 시작으로 돗자리가 자리가 있는지 확인 
+    def can_fit_mat(x, y, size):
         for dx in range(size):
             for dy in range(size):
-                nx = x + dx
-                ny = y + dy
-                if not(0 <= nx < n and 0 <= ny < m) or park[nx][ny] != "-1":
+                # 돗자리 안되면 False 반환
+                if park[x+dx][y+dy] != "-1":
                     return False
         return True
-
-
+                        
+    
     n = len(park)
     m = len(park[0])
-    # 돗자리 내림차순 정렬
-    mats.sort(reverse=True)
-    
-    # 가능한 돗자리 최대 크기 저장 
-    answer = -1
-    # 가능한 최대 크기 돗자리 인덱스 저장
-    idx = len(mats) - 1
-    
-    # 공원 순회
-    for i in range(n):
-        for j in range(m):
-            # 빈 자리 나오면 어떤 크기의 돗자리 가능한지 확인
-            if park[i][j] == "-1":
-                # 돗자리 크기 내림 차순으로 확인
-                for k in range(idx+1):
-                    # x 크기 돗자리 가능하면 갱신
-                    if check_mat_space(i, j, mats[k]):
-                        # 가장 큰 돗자리면 돗자리 크기 바로 반환
-                        if k == 0:
-                            return mats[k]
-                        # 인덱스 갱신 (다음 탐색은 더 큰 사이즈만 함)
-                        idx = min(idx, k-1)
-                        # 돗자리 크기 갱신
-                        answer = max(answer, mats[k]) 
-    return answer
+    # 돗자리 내림차순 정렬    
+    for mat in sorted(mats, reverse=True):
+        if can_place_mat(mat):
+            return mat
+        
+    return -1
