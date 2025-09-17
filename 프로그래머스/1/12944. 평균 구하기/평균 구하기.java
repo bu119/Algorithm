89a69f9f97f902a7d41c0af37e43b4adb89b70e9
bu119 <109335452+bu119@users.noreply.github.com>
2025-09-17 10:41:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,9 @@
-import java.util.Arrays;
-
 class Solution {
     public double solution(int[] arr) {
-        int sum = Arrays.stream(arr).sum(); // 배열 합
-        
-        return (double) sum / arr.length;   // 평균
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return (double) sum / arr.length; 
     }
 }
