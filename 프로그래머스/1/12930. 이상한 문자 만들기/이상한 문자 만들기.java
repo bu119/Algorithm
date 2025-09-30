1575edb7e44e87e6a93a237114397e4db517064d
bu119 <109335452+bu119@users.noreply.github.com>
2025-09-30 10:38:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.12 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -3,18 +3,14 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         // 단어(공백을 기준)별로 짝/홀수 인덱스를 판단
         int wordIdx = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i); // i번째 문자 꺼내기
+        for (char c : s.toCharArray()) {
             if (c == ' ') {
                 wordIdx = 0;
                 sb.append(c);
             }
-            else if (wordIdx % 2 == 0) {
-                sb.append(Character.toUpperCase(c));
-                wordIdx += 1;
-            } else {
-                sb.append(Character.toLowerCase(c));
-                wordIdx += 1;
+            else {
+                sb.append(wordIdx % 2 == 0 ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                wordIdx++;
             }
         }
         return sb.toString();
